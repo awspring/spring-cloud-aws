949cadb57171d870db5809153e54207aa4f9827d
Alain Sahli <sahli.alain@gmail.com>
2013-01-02 15:01:31 +0100

    Changed the gradle build and some tests to less environment specific.

--- elasticspring-context/src/test/java/org/elasticspring/CloudFormationAwsTest.java
+++ elasticspring-context/src/test/java/org/elasticspring/CloudFormationAwsTest.java
@@ -18,7 +18,7 @@
 
 package org.elasticspring;
 
-import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.cloudformation.AmazonCloudFormation;
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
 import com.amazonaws.services.cloudformation.model.DeleteStackRequest;
@@ -29,25 +29,30 @@ import com.amazonaws.services.cloudformation.model.Stack;
 import com.amazonaws.services.cloudformation.model.StackResource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.annotation.IfProfileValue;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.io.File;
 
 /**
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class CloudFormationAwsTest {
 
 	private AmazonCloudFormation amazonCloudFormation;
 
+	@Value("#{credentials.accessKey}")
+	private String accessKey;
+
+	@Value("#{credentials.secretKey}")
+	private String secretKey;
+
 	@Test
 	@IfProfileValue(name = "test-groups", value = "aws-test")
 	public void testGetConfiguration() throws Exception {
-		File file = new ClassPathResource("access.properties").getFile();
-		this.amazonCloudFormation = new AmazonCloudFormationClient(new PropertiesCredentials(file));
+		this.amazonCloudFormation = new AmazonCloudFormationClient(new BasicAWSCredentials(this.accessKey, this.secretKey));
 		this.amazonCloudFormation.setEndpoint("https://cloudformation.us-east-1.amazonaws.com");
 
 //		CreateStackResult sample = this.amazonCloudFormation.createStack(new CreateStackRequest().withStackName("sample").withTemplateURL("https://s3.amazonaws.com/cloudformation-templates-us-east-1/ElasticBeanstalk-1.0.0.template"));
@@ -66,8 +71,7 @@ public class CloudFormationAwsTest {
 	@Test
 	@IfProfileValue(name = "test-groups", value = "aws-test")
 	public void testDeleteStack() throws Exception {
-		File file = new ClassPathResource("access.properties").getFile();
-		this.amazonCloudFormation = new AmazonCloudFormationClient(new PropertiesCredentials(file));
+		this.amazonCloudFormation = new AmazonCloudFormationClient(new BasicAWSCredentials(this.accessKey, this.secretKey));
 		this.amazonCloudFormation.setEndpoint("https://cloudformation.us-east-1.amazonaws.com");
 
 		this.amazonCloudFormation.deleteStack(new DeleteStackRequest().withStackName("sample"));
