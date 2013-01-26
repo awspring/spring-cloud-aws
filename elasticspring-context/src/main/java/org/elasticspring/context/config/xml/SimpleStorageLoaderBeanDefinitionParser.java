19b42e6a89240e17bdae8940acddaa26d43419cc
Agim Emruli <agimemruli@gmail.com>
2013-01-27 00:27:13 +0100

    Fixed inspection warnings

--- elasticspring-context/src/main/java/org/elasticspring/context/config/xml/SimpleStorageLoaderBeanDefinitionParser.java
+++ elasticspring-context/src/main/java/org/elasticspring/context/config/xml/SimpleStorageLoaderBeanDefinitionParser.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2010-2012 the original author or authors.
+ * Copyright 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,6 +30,7 @@ import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
+@SuppressWarnings({"UnusedDeclaration", "WeakerAccess"})
 public class SimpleStorageLoaderBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
 
 	public static final String AMAZON_S3_BEAN_NAME = "AMAZON_S3";
