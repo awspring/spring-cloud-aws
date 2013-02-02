845becd0dca9fe6753888da782f30403669341b4
Agim Emruli <agimemruli@gmail.com>
2013-02-02 19:23:58 +0100

    Integration Tests are skipped on module level via gradle and not through the test profile anymore

--- elasticspring-integration-test/src/test/java/org/elasticspring/messaging/listener/SimpleMessageListenerContainerAwsTest.java
+++ elasticspring-integration-test/src/test/java/org/elasticspring/messaging/listener/SimpleMessageListenerContainerAwsTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2010-2012 the original author or authors.
+ * Copyright 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,7 +29,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
-import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -64,7 +63,6 @@ public class SimpleMessageListenerContainerAwsTest {
 	}
 
 	@Test
-	@IfProfileValue(name = "test-groups", value = "aws-test")
 	public void testSimpleListen() throws Exception {
 		final CountDownLatch messageReceivedCount = new CountDownLatch(100);
 		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
