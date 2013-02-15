bbaabc2017d59d4f71a2baa2d37e9b1bd78cc591
Agim Emruli <agimemruli@gmail.com>
2013-02-15 17:54:34 +0100

    Fixed different inspections

--- elasticspring-messaging/src/test/java/org/elasticspring/messaging/support/converter/ObjectMessageConverterTest.java
+++ elasticspring-messaging/src/test/java/org/elasticspring/messaging/support/converter/ObjectMessageConverterTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2010-2012 the original author or authors.
+ * Copyright 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -33,7 +33,7 @@ import java.nio.charset.UnsupportedCharsetException;
 public class ObjectMessageConverterTest {
 
 	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
+	public final ExpectedException expectedException = ExpectedException.none();
 
 	@Test
 	public void testToMessageAndFromMessage() throws Exception {
