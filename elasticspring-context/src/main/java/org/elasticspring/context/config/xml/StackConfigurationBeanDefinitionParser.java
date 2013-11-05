c6ffb033193c2f9fcb9e69699996d702eef4de8b
Christian Stettler <cstettler@imap.ch>
2013-11-05 17:06:27 +0100

    Added xml namespace support for stack configuration based on stack name auto-detection

--- elasticspring-context/src/main/java/org/elasticspring/context/config/xml/StackConfigurationBeanDefinitionParser.java
+++ elasticspring-context/src/main/java/org/elasticspring/context/config/xml/StackConfigurationBeanDefinitionParser.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
 
 import static org.springframework.beans.factory.support.BeanDefinitionBuilder.genericBeanDefinition;
@@ -20,20 +21,24 @@ class StackConfigurationBeanDefinitionParser extends AbstractSimpleBeanDefinitio
 
 	private static final String STACK_RESOURCE_REGISTRY_FACTORY_BEAN_CLASS_NAME = "org.elasticspring.core.env.stack.config.StackResourceRegistryFactoryBean";
 	private static final String STATIC_STACK_NAME_PROVIDER_CLASS_NAME = "org.elasticspring.core.env.stack.config.StaticStackNameProvider";
+	private static final String AUTO_DETECTING_STACK_NAME_PROVIDER_CLASS_NAME = "org.elasticspring.core.env.stack.config.AutoDetectingStackNameProvider";
+	private static final String INSTANCE_ID_PROVIDER_CLASS_NAME = "org.elasticspring.core.env.ec2.AmazonEC2InstanceIdProvider";
 
 	private static final String STACK_NAME_ATTRIBUTE_NAME = "stack-name";
 
 	@Override
 	protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
+		String amazonCloudFormationClientBeanName = buildAndRegisterAmazonCloudFormationClientBeanDefinition(parserContext);
+
 		String stackName = element.getAttribute(STACK_NAME_ATTRIBUTE_NAME);
 
-		builder.addConstructorArgValue(buildAmazonCloudFormationClientBeanDefinition());
-		builder.addConstructorArgValue(buildStaticStackNameProviderBeanDefinitionBuilder(stackName));
+		builder.addConstructorArgReference(amazonCloudFormationClientBeanName);
+		builder.addConstructorArgValue(StringUtils.isEmpty(stackName) ? buildAutoDetectingStackNameProviderBeanDefinition(amazonCloudFormationClientBeanName) : buildStaticStackNameProviderBeanDefinition(stackName));
 	}
 
 	@Override
 	protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext) throws BeanDefinitionStoreException {
-		return element.getAttribute(STACK_NAME_ATTRIBUTE_NAME);
+		return element.hasAttribute(STACK_NAME_ATTRIBUTE_NAME) ? element.getAttribute(STACK_NAME_ATTRIBUTE_NAME) : parserContext.getReaderContext().generateBeanName(definition);
 	}
 
 	@Override
@@ -41,11 +46,27 @@ class StackConfigurationBeanDefinitionParser extends AbstractSimpleBeanDefinitio
 		return STACK_RESOURCE_REGISTRY_FACTORY_BEAN_CLASS_NAME;
 	}
 
-	private static AbstractBeanDefinition buildStaticStackNameProviderBeanDefinitionBuilder(String stackName) {
+	private static AbstractBeanDefinition buildStaticStackNameProviderBeanDefinition(String stackName) {
 		BeanDefinitionBuilder staticStackNameProviderBeanDefinitionBuilder = genericBeanDefinition(STATIC_STACK_NAME_PROVIDER_CLASS_NAME);
 		staticStackNameProviderBeanDefinitionBuilder.addConstructorArgValue(stackName);
 
 		return staticStackNameProviderBeanDefinitionBuilder.getBeanDefinition();
+	}
+
+	private static AbstractBeanDefinition buildAutoDetectingStackNameProviderBeanDefinition(String amazonCloudFormationClientBeanName) {
+		BeanDefinitionBuilder autoDetectingStackNameProviderBeanDefinitionBuilder = genericBeanDefinition(AUTO_DETECTING_STACK_NAME_PROVIDER_CLASS_NAME);
+		autoDetectingStackNameProviderBeanDefinitionBuilder.addConstructorArgReference(amazonCloudFormationClientBeanName);
+		autoDetectingStackNameProviderBeanDefinitionBuilder.addConstructorArgValue(buildInstanceIdProviderBeanDefinition());
+
+		return autoDetectingStackNameProviderBeanDefinitionBuilder.getBeanDefinition();
+	}
+
+	private String buildAndRegisterAmazonCloudFormationClientBeanDefinition(ParserContext parserContext) {
+		AbstractBeanDefinition amazonCloudFormationClientBeanDefinition = buildAmazonCloudFormationClientBeanDefinition();
+		String amazonCloudFormationClientBeanName = parserContext.getReaderContext().generateBeanName(amazonCloudFormationClientBeanDefinition);
+		parserContext.getRegistry().registerBeanDefinition(amazonCloudFormationClientBeanName, amazonCloudFormationClientBeanDefinition);
+
+		return amazonCloudFormationClientBeanName;
 	}
 
 	private static AbstractBeanDefinition buildAmazonCloudFormationClientBeanDefinition() {
@@ -53,6 +74,12 @@ class StackConfigurationBeanDefinitionParser extends AbstractSimpleBeanDefinitio
 		amazonCloudFormationClientBeanDefinitionBuilder.addConstructorArgReference(CredentialsProviderFactoryBean.CREDENTIALS_PROVIDER_BEAN_NAME);
 
 		return amazonCloudFormationClientBeanDefinitionBuilder.getBeanDefinition();
+	}
+
+	private static AbstractBeanDefinition buildInstanceIdProviderBeanDefinition() {
+		BeanDefinitionBuilder instanceIdProviderBeanDefinitionBuilder = genericBeanDefinition(INSTANCE_ID_PROVIDER_CLASS_NAME);
+
+		return instanceIdProviderBeanDefinitionBuilder.getBeanDefinition();
 	}
 
 }
--- elasticspring-integration-test/src/test/java/org/elasticspring/core/env/stack/StackResourceRegistryAwsTest.java
+++ elasticspring-integration-test/src/test/java/org/elasticspring/core/env/stack/StackResourceRegistryAwsTest.java
@@ -1,16 +1,29 @@
 package org.elasticspring.core.env.stack;
 
-import org.elasticspring.support.TestStackEnvironment;
+import org.junit.After;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 // TODO change to test against resource id resolver (not stack resource registry)
 public class StackResourceRegistryAwsTest {
+
+	private final List<ClassPathXmlApplicationContext> loadedApplicationContexts = new ArrayList<ClassPathXmlApplicationContext>();
+
+	@After
+	public void destroyApplicationContexts() {
+		for (ClassPathXmlApplicationContext applicationContext : this.loadedApplicationContexts) {
+			applicationContext.close();
+		}
+	}
 
 	@Test
 	public void stackResourceRegistry_staticStackNameProvider_stackResourceRegistryBeanExposedUnderStackName() {
@@ -18,19 +31,19 @@ public class StackResourceRegistryAwsTest {
 		ClassPathXmlApplicationContext applicationContext = loadApplicationContext("staticStackName");
 
 		// Act
-		StackResourceRegistry staticStackNameProviderBasedStackResourceRegistry = applicationContext.getBean(TestStackEnvironment.DEFAULT_STACK_NAME, StackResourceRegistry.class);
+		StackResourceRegistry staticStackNameProviderBasedStackResourceRegistry = applicationContext.getBean("IntegrationTestStack", StackResourceRegistry.class);
 
 		// Assert
 		assertThat(staticStackNameProviderBasedStackResourceRegistry, is(not(nullValue())));
 	}
 
 	@Test
-	public void stackResourceRegistry_autoDetectingStackNameProvider_stackResourceRegistryBeanExposed() {
+	public void stackResourceRegistry_autoDetectingStackNameProvider_stackResourceRegistryBeanExposedUnderGeneratedName() {
 		// Arrange
 		ClassPathXmlApplicationContext applicationContext = loadApplicationContext("autoDetectStackName");
 
 		// Act
-		StackResourceRegistry autoDetectingStackNameProviderBasedStackResourceRegistry = applicationContext.getBean(StackResourceRegistry.class);
+		StackResourceRegistry autoDetectingStackNameProviderBasedStackResourceRegistry = applicationContext.getBean("org.elasticspring.core.env.stack.config.StackResourceRegistryFactoryBean#0", StackResourceRegistry.class);
 
 		// Assert
 		assertThat(autoDetectingStackNameProviderBasedStackResourceRegistry, is(not(nullValue())));
@@ -43,10 +56,10 @@ public class StackResourceRegistryAwsTest {
 		StackResourceRegistry staticStackNameProviderBasedStackResourceRegistry = applicationContext.getBean(StackResourceRegistry.class);
 
 		// Act
-		String physicalResourceId = staticStackNameProviderBasedStackResourceRegistry.lookupPhysicalResourceId("RdsSingleMicroInstance");
+		String physicalResourceId = staticStackNameProviderBasedStackResourceRegistry.lookupPhysicalResourceId("EmptyBucket");
 
 		// Assert
-		assertThat(physicalResourceId, is(not(nullValue())));
+		assertThat(physicalResourceId, startsWith("integrationteststack-emptybucket-"));
 	}
 
 	@Test
@@ -56,10 +69,10 @@ public class StackResourceRegistryAwsTest {
 		StackResourceRegistry staticStackNameProviderBasedStackResourceRegistry = applicationContext.getBean(StackResourceRegistry.class);
 
 		// Act
-		String physicalResourceId = staticStackNameProviderBasedStackResourceRegistry.lookupPhysicalResourceId("RdsSingleMicroInstance");
+		String physicalResourceId = staticStackNameProviderBasedStackResourceRegistry.lookupPhysicalResourceId("EmptyBucket");
 
 		// Assert
-		assertThat(physicalResourceId, is(not(nullValue())));
+		assertThat(physicalResourceId, startsWith("integrationteststack-emptybucket-"));
 	}
 
 	@Test
@@ -75,8 +88,11 @@ public class StackResourceRegistryAwsTest {
 		assertThat(physicalResourceId, is(nullValue()));
 	}
 
-	private static ClassPathXmlApplicationContext loadApplicationContext(String configurationName) {
-		return new ClassPathXmlApplicationContext(StackResourceRegistryAwsTest.class.getSimpleName() + "-" + configurationName + ".xml", StackResourceRegistryAwsTest.class);
+	private ClassPathXmlApplicationContext loadApplicationContext(String configurationName) {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(StackResourceRegistryAwsTest.class.getSimpleName() + "-" + configurationName + ".xml", StackResourceRegistryAwsTest.class);
+		this.loadedApplicationContexts.add(applicationContext);
+
+		return applicationContext;
 	}
 
 }
