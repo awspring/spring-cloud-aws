ab9bb3eb2030feb881f8431339b44ada5e573070
Agim Emruli <agimemruli@gmail.com>
2014-03-26 21:10:07 +0100

    Replaced import and static linking to external classes with constants to avoid external dependencies in bean definition parser

--- elasticspring-context/src/main/java/org/elasticspring/cache/config/xml/CacheBeanDefinitionParser.java
+++ elasticspring-context/src/main/java/org/elasticspring/cache/config/xml/CacheBeanDefinitionParser.java
@@ -16,12 +16,6 @@
 
 package org.elasticspring.cache.config.xml;
 
-import com.google.code.ssm.CacheFactory;
-import com.google.code.ssm.config.DefaultAddressProvider;
-import com.google.code.ssm.providers.CacheConfiguration;
-import com.google.code.ssm.providers.spymemcached.MemcacheClientFactoryImpl;
-import com.google.code.ssm.spring.SSMCacheManager;
-import org.elasticspring.cache.config.ElastiCacheAddressProvider;
 import org.elasticspring.cache.config.SsmCacheFactoryBean;
 import org.elasticspring.context.config.xml.GlobalBeanDefinitionUtils;
 import org.elasticspring.context.config.xml.support.AmazonWebserviceClientConfigurationUtils;
@@ -54,10 +48,26 @@ class CacheBeanDefinitionParser extends AbstractBeanDefinitionParser {
 	private static final String CACHE_REF_ELEMENT_NAME = "cache-ref";
 	private static final String CACHE_ELEMENT_NAME = "cache";
 
+	private static final String CACHE_FACTORY_CLASS_NAME = "com.google.code.ssm.CacheFactory";
+	private static final String CACHE_CONFIGURATION_CLASS_NAME = "com.google.code.ssm.providers.CacheConfiguration";
+	private static final String DEFAULT_ADDRESS_PROVIDER_CLASS_NAME = "com.google.code.ssm.config.DefaultAddressProvider";
+	private static final String ELASTICACHE_ADDRESS_PROVIDER_CLASS_NAME = "org.elasticspring.cache.config.ElastiCacheAddressProvider";
+	private static final String MEMCACHE_CLIENT_CLASS_NAME = "com.google.code.ssm.providers.spymemcached.MemcacheClientFactoryImpl";
+	private static final String SSM_CACHE_MANAGER_CLASS_NAME = "com.google.code.ssm.spring.SSMCacheManager";
+
+	private static String getRequiredAttribute(String attributeName, Element source, ParserContext parserContext) {
+		if (StringUtils.hasText(source.getAttribute(attributeName))) {
+			return source.getAttribute(attributeName);
+		} else {
+			parserContext.getReaderContext().error("Attribute '" + attributeName + "' is required", source);
+			return null;
+		}
+	}
+
 	@Override
 	protected AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
 		if (!parserContext.getRegistry().containsBeanDefinition(CACHE_MANAGER)) {
-			BeanDefinitionBuilder cacheManagerDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(SSMCacheManager.class);
+			BeanDefinitionBuilder cacheManagerDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(SSM_CACHE_MANAGER_CLASS_NAME);
 			cacheManagerDefinitionBuilder.addPropertyValue("caches", createCacheCollection(element, parserContext));
 			parserContext.getRegistry().registerBeanDefinition(CACHE_MANAGER, cacheManagerDefinitionBuilder.getBeanDefinition());
 		} else {
@@ -102,7 +112,7 @@ class CacheBeanDefinitionParser extends AbstractBeanDefinitionParser {
 	}
 
 	private BeanDefinition createCache(String name, BeanDefinition addressProvider) {
-		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(CacheFactory.class);
+		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(CACHE_FACTORY_CLASS_NAME);
 		beanDefinitionBuilder.addPropertyValue("cacheName", name);
 		beanDefinitionBuilder.addPropertyValue("cacheClientFactory", createClientFactoryImpl());
 		beanDefinitionBuilder.addPropertyValue("addressProvider", addressProvider);
@@ -112,22 +122,22 @@ class CacheBeanDefinitionParser extends AbstractBeanDefinitionParser {
 	}
 
 	private AbstractBeanDefinition createCacheConfiguration() {
-		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(CacheConfiguration.class);
+		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(CACHE_CONFIGURATION_CLASS_NAME);
 		// TODO why do we set this flag to true? Shouldn't this be configurable?
 		beanDefinitionBuilder.addPropertyValue("consistentHashing", true);
 		return beanDefinitionBuilder.getBeanDefinition();
 	}
 
 	private BeanDefinition createDefaultAddressProvider(String address) {
-		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(DefaultAddressProvider.class);
+		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(DEFAULT_ADDRESS_PROVIDER_CLASS_NAME);
 		beanDefinitionBuilder.addConstructorArgValue(address);
 		return beanDefinitionBuilder.getBeanDefinition();
 	}
 
 	private BeanDefinition createElastiCacheAddressProvider(BeanDefinitionRegistry beanDefinitionRegistry, Element source, String clusterId) {
 		BeanDefinitionHolder elastiCacheClient = AmazonWebserviceClientConfigurationUtils.registerAmazonWebserviceClient(beanDefinitionRegistry,
-				"com.amazonaws.services.elasticache.AmazonElastiCacheClient",source.getAttribute("region-provider"),source.getAttribute("region"));
-		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(ElastiCacheAddressProvider.class);
+				"com.amazonaws.services.elasticache.AmazonElastiCacheClient", source.getAttribute("region-provider"), source.getAttribute("region"));
+		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(ELASTICACHE_ADDRESS_PROVIDER_CLASS_NAME);
 		beanDefinitionBuilder.addConstructorArgReference(elastiCacheClient.getBeanName());
 		beanDefinitionBuilder.addConstructorArgReference(GlobalBeanDefinitionUtils.retrieveResourceIdResolverBeanName(beanDefinitionRegistry));
 		beanDefinitionBuilder.addConstructorArgValue(clusterId);
@@ -135,15 +145,6 @@ class CacheBeanDefinitionParser extends AbstractBeanDefinitionParser {
 	}
 
 	private AbstractBeanDefinition createClientFactoryImpl() {
-		return BeanDefinitionBuilder.rootBeanDefinition(MemcacheClientFactoryImpl.class).getBeanDefinition();
-	}
-
-	private static String getRequiredAttribute(String attributeName, Element source, ParserContext parserContext) {
-		if (StringUtils.hasText(source.getAttribute(attributeName)) ) {
-			return source.getAttribute(attributeName);
-		}else{
-			parserContext.getReaderContext().error("Attribute '" + attributeName + "' is required", source);
-			return null;
-		}
+		return BeanDefinitionBuilder.rootBeanDefinition(MEMCACHE_CLIENT_CLASS_NAME).getBeanDefinition();
 	}
 }
