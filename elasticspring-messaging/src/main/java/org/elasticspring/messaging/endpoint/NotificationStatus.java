541ed10fb5d083395fdb638b7e8a7c88dfd08564
Agim Emruli <agimemruli@gmail.com>
2014-06-30 18:35:47 +0200

    Removed cancelSubscription as this is not needed in the SNS lifecycle of a controller

--- elasticspring-messaging/src/main/java/org/elasticspring/messaging/endpoint/NotificationStatus.java
+++ elasticspring-messaging/src/main/java/org/elasticspring/messaging/endpoint/NotificationStatus.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2013-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,5 +23,4 @@ public interface NotificationStatus {
 
 	void confirmSubscription();
 
-	void cancelSubscription();
 }
