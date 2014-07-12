4680165ea13da8cd9b19422a30aa9d0f0f221c0f
Agim Emruli <agimemruli@gmail.com>
2014-07-12 19:40:00 +0200

    Added support for custom amazon rds for retry interceptor Issue spring-cloud/spring-cloud-aws#16

--- elasticspring-jdbc/src/test/java/org/elasticspring/jdbc/config/xml/AmazonRdsRetryInterceptorBeanDefinitionParserTest.java
+++ elasticspring-jdbc/src/test/java/org/elasticspring/jdbc/config/xml/AmazonRdsRetryInterceptorBeanDefinitionParserTest.java
@@ -17,7 +17,9 @@
 package org.elasticspring.jdbc.config.xml;
 
 import com.amazonaws.services.rds.AmazonRDS;
+import com.amazonaws.services.rds.AmazonRDSClient;
 import org.aopalliance.intercept.MethodInterceptor;
+import org.elasticspring.config.AmazonWebserviceClientConfigurationUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
@@ -27,67 +29,90 @@ import org.springframework.test.util.ReflectionTestUtils;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 /**
- *
+ * @author Agim Emruli
  */
 public class AmazonRdsRetryInterceptorBeanDefinitionParserTest {
 
 	@Test
-	public void testCreateMinimalConfiguration() throws Exception {
+	public void parseInternal_minimalConfiguration_createsRetryInterceptor() throws Exception {
+		//Arrange
 		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(getClass().getSimpleName() + "-minimal.xml", getClass());
 
-		//Get bean so it will be initialized
+		//Act
 		MethodInterceptor interceptor = classPathXmlApplicationContext.getBean(MethodInterceptor.class);
 
+		//Assert
 		assertNotNull(interceptor);
-
 	}
 
 
 	@Test
-	public void testCreateCustomRegion() throws Exception {
+	public void parseInternal_customRegionConfigured_createsAmazonRdsClientWithCustomRegionConfigured() throws Exception {
+		//Arrange
 		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(getClass().getSimpleName() + "-customRegion.xml", getClass());
 
-		//Get bean so it will be initialized
+		//Act
 		AmazonRDS amazonRDS = classPathXmlApplicationContext.getBean(AmazonRDS.class);
 
-		//have to use reflection utils
+		//Assert
 		assertEquals("https://rds.eu-west-1.amazonaws.com", ReflectionTestUtils.getField(amazonRDS, "endpoint").toString());
-
 	}
 
 
 	@Test
-	public void testCreateCustomRegionProvider() throws Exception {
+	public void parseInternal_customRegionProviderConfigured_createAmazonRdsClientWithCustomRegionConfigured() throws Exception {
+		//Arrange
 		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(getClass().getSimpleName() + "-customRegionProvider.xml", getClass());
 
-		//Get bean so it will be initialized
+		//Act
 		AmazonRDS amazonRDS = classPathXmlApplicationContext.getBean(AmazonRDS.class);
 
-		//have to use reflection utils
+		//Assert
 		assertEquals("https://rds.eu-west-1.amazonaws.com", ReflectionTestUtils.getField(amazonRDS, "endpoint").toString());
-
 	}
 
 	@Test
-	public void testCustomBackoffPolicy() throws Exception {
+	public void parseInternal_customRDsClientConfigured_createInterceptorWithCustomRdsClient() throws Exception {
+		//Arrange
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(getClass().getSimpleName() + "-customRdsClient.xml", getClass());
+
+		//Act
+		classPathXmlApplicationContext.getBean(MethodInterceptor.class);
+
+		//Assert
+		assertFalse(classPathXmlApplicationContext.containsBean(AmazonWebserviceClientConfigurationUtils.getBeanName(AmazonRDSClient.class.getName())));
+	}
+
+	@Test
+	public void parseInternal_customBackOffPolicy_createInterceptorWithCustomBackOffPolicy() throws Exception {
+		//Arrange
 		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(getClass().getSimpleName() + "-customBackOffPolicy.xml", getClass());
 		BeanDefinition beanDefinition = classPathXmlApplicationContext.getBeanFactory().getBeanDefinition("interceptor");
+
+		//Act
 		BeanDefinition retryOperations = (BeanDefinition) beanDefinition.getPropertyValues().getPropertyValue("retryOperations").getValue();
+
+		//Assert
 		assertEquals("policy", ((RuntimeBeanReference) retryOperations.getPropertyValues().getPropertyValue("backOffPolicy").getValue()).getBeanName());
 	}
 
 	@Test
-	public void testCustomMaxNumberOfRetries() throws Exception {
+	public void parseInternal_customNumberOfRetiresConfigured_createRetryPolicyWithCustomNumberOfRetriesConfigured() throws Exception {
+		//Arrange
 		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(getClass().getSimpleName() + "-maxNumberOfRetries.xml", getClass());
+
+		//Act
 		BeanDefinition beanDefinition = classPathXmlApplicationContext.getBeanFactory().getBeanDefinition("interceptor");
+
+		//Assert
 		BeanDefinition retryOperations = (BeanDefinition) beanDefinition.getPropertyValues().getPropertyValue("retryOperations").getValue();
 		BeanDefinition compositeRetryPolicy = (BeanDefinition) retryOperations.getPropertyValues().getPropertyValue("retryPolicy").getValue();
 		@SuppressWarnings("unchecked") List<BeanDefinition> policies = (List<BeanDefinition>) compositeRetryPolicy.getPropertyValues().getPropertyValue("policies").getValue();
 		BeanDefinition sqlPolicy = policies.get(1);
 		assertEquals("4", sqlPolicy.getPropertyValues().getPropertyValue("maxNumberOfRetries").getValue().toString());
-
 	}
 }
