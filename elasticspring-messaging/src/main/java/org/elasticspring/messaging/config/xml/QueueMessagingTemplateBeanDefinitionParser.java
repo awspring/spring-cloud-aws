b6fdf824a6d9a187294cd60f1336ef4c6bbe9c1d
Agim Emruli <agimemruli@gmail.com>
2014-07-13 16:11:34 +0200

    Removed strictContentTypeMatch for messaging templates Used interface AmazonSQSAsync interface instead of concrete client AmazonSQSAsyncClient

--- elasticspring-messaging/src/main/java/org/elasticspring/messaging/config/xml/QueueMessagingTemplateBeanDefinitionParser.java
+++ elasticspring-messaging/src/main/java/org/elasticspring/messaging/config/xml/QueueMessagingTemplateBeanDefinitionParser.java
@@ -67,7 +67,6 @@ public class QueueMessagingTemplateBeanDefinitionParser extends AbstractSingleBe
 	private void registerJacksonMessageConverter(BeanDefinitionBuilder builder) {
 		BeanDefinitionBuilder jacksonBeanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition("org.springframework.messaging.converter.MappingJackson2MessageConverter");
 		jacksonBeanDefinitionBuilder.addPropertyValue("serializedPayloadClass", String.class);
-		jacksonBeanDefinitionBuilder.addPropertyValue("strictContentTypeMatch", true);
 		builder.addPropertyValue(
 				Conventions.attributeNameToPropertyName(MESSAGE_CONVERTER_ATTRIBUTE), jacksonBeanDefinitionBuilder.getBeanDefinition());
 	}
