f280f8045ac4dc8058d7f1e198d39cbea6fa85f7
Dave Syer <dsyer@pivotal.io>
2016-02-12 11:35:33 +0000

    Align travis build with tools in spring-cloud/build

--- travis-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-if [[ ($TRAVIS_BRANCH =~ ^.*\/.*$) || ($TRAVIS_PULL_REQUEST != false) ]]
-  then
-  	mvn --settings .settings.xml test -q -U -Dmaven.test.redirectTestOutputToFile=true
-  else
-    mvn --settings .settings.xml install -P docs -q -U -DskipTests=true -Dmaven.test.redirectTestOutputToFile=true
-	./docs/src/main/asciidoc/ghpages.sh
-	mvn --settings .settings.xml deploy -nsu -Dmaven.test.redirectTestOutputToFile=true
-fi
