b01aa5c888a4a6413b3ca0d222a8fa7ba4b1c638
szymondembek <szymon.dembek@ziflow.com>
2017-04-29 21:54:01 +0200

    minor copyright fixes

--- spring-cloud-aws-messaging/src/test/java/org/springframework/cloud/aws/messaging/listener/support/VisibilityHandlerMethodArgumentResolverTest.java
+++ spring-cloud-aws-messaging/src/test/java/org/springframework/cloud/aws/messaging/listener/support/VisibilityHandlerMethodArgumentResolverTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2014 the original author or authors.
+ * Copyright 2013-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,8 +23,8 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
 /**
- * @author Alain Sahli
- * @since 1.1
+ * @author Szymon Dembek
+ * @since 1.3
  */
 public class VisibilityHandlerMethodArgumentResolverTest {
 
