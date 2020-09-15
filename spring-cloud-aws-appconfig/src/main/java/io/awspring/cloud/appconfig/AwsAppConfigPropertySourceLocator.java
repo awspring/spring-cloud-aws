68e09e3cc8c8db4dec3a2762823b8489528651fd
jarp <jonathanrpx@gmail.com>
2020-09-15 16:29:54 -0500

    feat(aws-appconfig): move classes & fix comments

---
+++ spring-cloud-aws-appconfig/src/main/java/io/awspring/cloud/appconfig/AwsAppConfigPropertySourceLocator.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.awspring.cloud.appconfig;
+
+import com.amazonaws.services.appconfig.AmazonAppConfig;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
+import org.springframework.core.env.CompositePropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertySource;
+import org.springframework.util.ReflectionUtils;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @author jarpz
+ */
+public class AwsAppConfigPropertySourceLocator implements PropertySourceLocator {
+
+	private final AmazonAppConfig appConfigClient;
+
+	private final String accountId;
+
+	private final String application;
+
+	private final String configurationProfile;
+
+	private final String environment;
+
+	private final String configurationVersion;
+
+	private final boolean failFast;
+
+	private static final Log logger = LogFactory.getLog(AwsAppConfigPropertySourceLocator.class);
+
+	public AwsAppConfigPropertySourceLocator(AmazonAppConfig appConfigClient, String accountId, String application,
+			String configurationProfile, String environment, String configurationVersion, boolean failFast) {
+		this.appConfigClient = appConfigClient;
+		this.accountId = accountId;
+		this.application = application;
+		this.configurationProfile = configurationProfile;
+		this.environment = environment;
+		this.configurationVersion = configurationVersion;
+		this.failFast = failFast;
+	}
+
+	@Override
+	public PropertySource<?> locate(Environment environment) {
+		if (!(environment instanceof ConfigurableEnvironment)) {
+			return null;
+		}
+
+		ConfigurableEnvironment env = (ConfigurableEnvironment) environment;
+
+		String appName = configurationProfile;
+		if (isNull(appName)) {
+			appName = requireNonNull(env.getProperty("spring.application.name"),
+					"configurationProfile should not be empty or null.");
+		}
+
+		CompositePropertySource composite = new CompositePropertySource("aws-app-config");
+
+		try {
+			composite.addPropertySource(create(appName));
+		}
+		catch (Exception ex) {
+			if (failFast) {
+				logger.error("Fail fast is set and there was an error reading configuration from AWS AppConfig: {}",
+						ex);
+				ReflectionUtils.rethrowRuntimeException(ex);
+			}
+			else {
+				logger.warn("Unable to load AWS AppConfig from " + appName, ex);
+			}
+		}
+
+		return composite;
+	}
+
+	private AwsAppConfigPropertySource create(String appName) {
+		AwsAppConfigPropertySource propertySource = new AwsAppConfigPropertySource(appName, accountId, application,
+				environment, configurationVersion, appConfigClient);
+		propertySource.init();
+
+		return propertySource;
+	}
+
+}
---
+++ spring-cloud-aws-appconfig/src/test/java/io/awspring/cloud/appconfig/AwsAppConfigPropertySourceLocatorTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.awspring.cloud.appconfig;
+
+import java.nio.ByteBuffer;
+
+import com.amazonaws.services.appconfig.AmazonAppConfig;
+import com.amazonaws.services.appconfig.model.GetConfigurationResult;
+import org.apache.commons.codec.Resources;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.core.env.PropertySource;
+import org.springframework.mock.env.MockEnvironment;
+
+import static com.amazonaws.util.IOUtils.toByteArray;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AwsAppConfigPropertySourceLocatorTest {
+
+	private AmazonAppConfig appConfigClient = mock(AmazonAppConfig.class);
+
+	private MockEnvironment env = new MockEnvironment();
+
+	@Test
+	void whenLoadYamlAppConfigThenReturnPropertySource() throws Exception {
+		ByteBuffer content = ByteBuffer.wrap(toByteArray(Resources.getInputStream("config.yaml")));
+
+		GetConfigurationResult result = new GetConfigurationResult();
+		result.setConfigurationVersion("1");
+		result.setContentType("application/x-yaml");
+		result.setContent(content);
+
+		when(appConfigClient.getConfiguration(any())).thenReturn(result);
+
+		AwsAppConfigPropertySourceLocator locator = new AwsAppConfigPropertySourceLocator(appConfigClient, "123456789",
+				"my-project", "api-my", "dev", null, false);
+
+		env.setActiveProfiles("test");
+
+		PropertySource<?> propertySource = locator.locate(env);
+
+		assertThat(propertySource.getProperty("server.port")).isEqualTo(8081);
+	}
+
+	@Test
+	void whenLoadJsonAppConfigThenReturnPropertySource() throws Exception {
+		ByteBuffer content = ByteBuffer.wrap(toByteArray(Resources.getInputStream("config.json")));
+
+		GetConfigurationResult result = new GetConfigurationResult();
+		result.setConfigurationVersion("1");
+		result.setContentType("application/json");
+		result.setContent(content);
+
+		when(appConfigClient.getConfiguration(any())).thenReturn(result);
+
+		AwsAppConfigPropertySourceLocator locator = new AwsAppConfigPropertySourceLocator(appConfigClient, "123456789",
+				"my-project", "api-my", "dev", null, false);
+
+		env.setActiveProfiles("test");
+
+		PropertySource<?> propertySource = locator.locate(env);
+
+		assertThat(propertySource.getProperty("server.port")).isEqualTo(8089);
+	}
+
+	@Test
+	void whenLoadAnAppConfigAndThrowErrorThenRethrowErrorWhenFailFastTrue() {
+		GetConfigurationResult result = new GetConfigurationResult();
+		result.setConfigurationVersion("1");
+		result.setContentType("application/json");
+
+		when(appConfigClient.getConfiguration(any())).thenThrow(new RuntimeException("connection error"));
+
+		AwsAppConfigPropertySourceLocator locator = new AwsAppConfigPropertySourceLocator(appConfigClient, "123456789",
+				"my-project", "api-my", "dev", null, true);
+
+		env.setActiveProfiles("test");
+
+		assertThatThrownBy(() -> locator.locate(env)).hasMessage("connection error");
+	}
+
+	@Test
+	void whenLoadAnAppConfigAndThrowErrorThenReturnEmptyWhenFailFastFalse() {
+		GetConfigurationResult result = new GetConfigurationResult();
+		result.setConfigurationVersion("1");
+		result.setContentType("application/json");
+
+		when(appConfigClient.getConfiguration(any())).thenThrow(new RuntimeException("connection error"));
+
+		AwsAppConfigPropertySourceLocator locator = new AwsAppConfigPropertySourceLocator(appConfigClient, "123456789",
+				"my-project", "api-my", "dev", null, false);
+
+		env.setActiveProfiles("test");
+
+		assertThatCode(() -> locator.locate(env)).doesNotThrowAnyException();
+	}
+
+}
