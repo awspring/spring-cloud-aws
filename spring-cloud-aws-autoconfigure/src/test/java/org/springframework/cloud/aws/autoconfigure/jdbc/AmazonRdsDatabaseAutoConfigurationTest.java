8532c84bc2ed7a44aa8b962ea14b0173d67beb94
Eddú Meléndez <eddu.melendez@gmail.com>
2020-12-08 18:49:59 -0600

    Remove public from tests

--- spring-cloud-aws-autoconfigure/src/test/java/org/springframework/cloud/aws/autoconfigure/jdbc/AmazonRdsDatabaseAutoConfigurationTest.java
+++ spring-cloud-aws-autoconfigure/src/test/java/org/springframework/cloud/aws/autoconfigure/jdbc/AmazonRdsDatabaseAutoConfigurationTest.java
@@ -48,7 +48,7 @@ class AmazonRdsDatabaseAutoConfigurationTest {
 			.withConfiguration(AutoConfigurations.of(AmazonRdsDatabaseAutoConfiguration.class));
 
 	@Test
-	public void registersRdsInstanceConfigurer() {
+	void registersRdsInstanceConfigurer() {
 		this.contextRunner.withUserConfiguration(ApplicationConfigurationWithoutReadReplica.class)
 				.withUserConfiguration(CustomRdsInstanceConfigurer.class)
 				.withPropertyValues("cloud.aws.rds.instances[0].dbInstanceIdentifier:test",
@@ -136,7 +136,7 @@ class AmazonRdsDatabaseAutoConfigurationTest {
 	}
 
 	@Test
-	public void configureBean_withoutPasswordSpecified_doNotConfigureFactoryBean() {
+	void configureBean_withoutPasswordSpecified_doNotConfigureFactoryBean() {
 		this.contextRunner.withPropertyValues("cloud.aws.rds.instances.instances[0].dbInstanceIdentifier:test")
 				.run((context) -> {
 					assertThat(context).doesNotHaveBean(DataSource.class);
