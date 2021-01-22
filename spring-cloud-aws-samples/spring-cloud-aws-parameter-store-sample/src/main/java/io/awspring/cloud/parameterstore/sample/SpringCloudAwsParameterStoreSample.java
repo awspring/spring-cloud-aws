5c15d3682831005740779f8fba9eed1ba3780ff6
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-01-23 00:32:07 +0100

    Fix imports.

--- spring-cloud-aws-samples/spring-cloud-aws-parameter-store-sample/src/main/java/io/awspring/cloud/parameterstore/sample/SpringCloudAwsParameterStoreSample.java
+++ spring-cloud-aws-samples/spring-cloud-aws-parameter-store-sample/src/main/java/io/awspring/cloud/parameterstore/sample/SpringCloudAwsParameterStoreSample.java
@@ -18,6 +18,7 @@ package io.awspring.cloud.parameterstore.sample;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
