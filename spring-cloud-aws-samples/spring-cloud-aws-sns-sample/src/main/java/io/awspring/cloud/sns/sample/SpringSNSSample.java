de025f1376935b2d4a8e0ae85291dd2d2a594c90
MatejNedic <matejnedic1@gmail.com>
2021-01-22 19:48:32 +0100

    unused import

--- spring-cloud-aws-samples/spring-cloud-aws-sns-sample/src/main/java/io/awspring/cloud/sns/sample/SpringSNSSample.java
+++ spring-cloud-aws-samples/spring-cloud-aws-sns-sample/src/main/java/io/awspring/cloud/sns/sample/SpringSNSSample.java
@@ -26,7 +26,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
 
