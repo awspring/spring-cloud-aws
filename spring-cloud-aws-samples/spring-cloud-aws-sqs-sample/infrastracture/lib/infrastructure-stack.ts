b01ae160491095681be2a9b384ee7112d0f7e992
MatejNedic <matejnedic1@gmail.com>
2021-02-08 19:55:55 +0100

    Fix samples (#43)

--- spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/infrastracture/lib/infrastructure-stack.ts
+++ spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/infrastracture/lib/infrastructure-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as sqs from '@aws-cdk/aws-sqs';
 
-export class InfrastractureStack extends cdk.Stack {
+export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
--- spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/infrastracture/test/infrastracture.test.ts
+++ spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/infrastracture/test/infrastracture.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as Infrastracture from '../lib/infrastracture-stack';
+import * as Infrastracture from '../lib/infrastructure-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Infrastracture.InfrastractureStack(app, 'MyTestStack');
+    const stack = new Infrastracture.InfrastructureStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
