48a74fd2ceeba95f1a46286c7a499c84af7d91f5
Arun Patra <arunkpatra@gmail.com>
2021-02-16 18:47:18 +0530

    #58 Migrate secrets manager to V2

---
+++ v3/spring-cloud-aws-v3-secrets-manager-config/src/main/java/io/awspring/cloud/v3/secretsmanager/AwsSecretsManagerProperties.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.awspring.cloud.v3.secretsmanager;
+
+import java.net.URI;
+import java.util.regex.Pattern;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+/**
+ * Configuration properties for the AWS Secrets Manager integration. Mostly based on the
+ * Spring Cloud Consul Configuration equivalent.
+ *
+ * @author Fabio Maia
+ * @author Matej Nedic
+ * @author Hari Ohm Prasath
+ * @author Arun Patra
+ * @since 2.0.0
+ */
+@ConfigurationProperties(prefix = AwsSecretsManagerProperties.CONFIG_PREFIX)
+public class AwsSecretsManagerProperties implements Validator {
+
+	/**
+	 * Configuration prefix.
+	 */
+	public static final String CONFIG_PREFIX = "aws.secretsmanager";
+
+	/**
+	 * Pattern used for prefix validation.
+	 */
+	private static final Pattern PREFIX_PATTERN = Pattern.compile("(/)?([a-zA-Z0-9.\\-_]+)*");
+
+	/**
+	 * Pattern used for profileSeparator validation.
+	 */
+	private static final Pattern PROFILE_SEPARATOR_PATTERN = Pattern.compile("[a-zA-Z0-9.\\-_/\\\\]+");
+
+	/**
+	 * Prefix indicating first level for every property. Value must start with a forward
+	 * slash followed by a valid path segment or be empty. Defaults to "/secret".
+	 */
+	private String prefix = "/secret";
+
+	private String defaultContext = "application";
+
+	private String profileSeparator = "_";
+
+	/** Throw exceptions during config lookup if true, otherwise, log warnings. */
+	private boolean failFast = true;
+
+	/**
+	 * If region value is not null or empty it will be used in creation of
+	 * AWSSecretsManager.
+	 */
+	private String region;
+
+	/**
+	 * Overrides the default endpoint.
+	 */
+	private URI endpoint;
+
+	/**
+	 * Alternative to spring.application.name to use in looking up values in AWS Secrets
+	 * Manager.
+	 */
+	private String name;
+
+	/** Is AWS Secrets Manager support enabled. */
+	private boolean enabled = true;
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return AwsSecretsManagerProperties.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		AwsSecretsManagerProperties properties = (AwsSecretsManagerProperties) target;
+
+		if (!StringUtils.hasLength(properties.getPrefix())) {
+			errors.rejectValue("prefix", "NotEmpty", "prefix should not be empty or null.");
+		}
+
+		if (!StringUtils.hasLength(properties.getDefaultContext())) {
+			errors.rejectValue("defaultContext", "NotEmpty", "defaultContext should not be empty or null.");
+		}
+
+		if (!StringUtils.hasLength(properties.getProfileSeparator())) {
+			errors.rejectValue("profileSeparator", "NotEmpty", "profileSeparator should not be empty or null.");
+		}
+
+		if (!PREFIX_PATTERN.matcher(properties.getPrefix()).matches()) {
+			errors.rejectValue("prefix", "Pattern", "The prefix must have pattern of:  " + PREFIX_PATTERN.toString());
+		}
+		if (!PROFILE_SEPARATOR_PATTERN.matcher(properties.getProfileSeparator()).matches()) {
+			errors.rejectValue("profileSeparator", "Pattern",
+					"The profileSeparator must have pattern of:  " + PROFILE_SEPARATOR_PATTERN.toString());
+		}
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getDefaultContext() {
+		return defaultContext;
+	}
+
+	public void setDefaultContext(String defaultContext) {
+		this.defaultContext = defaultContext;
+	}
+
+	public String getProfileSeparator() {
+		return profileSeparator;
+	}
+
+	public void setProfileSeparator(String profileSeparator) {
+		this.profileSeparator = profileSeparator;
+	}
+
+	public boolean isFailFast() {
+		return failFast;
+	}
+
+	public void setFailFast(boolean failFast) {
+		this.failFast = failFast;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(final String region) {
+		this.region = region;
+	}
+
+	public URI getEndpoint() {
+		return endpoint;
+	}
+
+	public void setEndpoint(URI endpoint) {
+		this.endpoint = endpoint;
+	}
+
+}
---
+++ v3/spring-cloud-aws-v3-secrets-manager-config/src/test/java/io/awspring/cloud/v3/secretsmanager/AwsSecretsManagerPropertySourceLocatorTest.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.awspring.cloud.v3.secretsmanager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.env.CompositePropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.mock.env.MockEnvironment;
+
+import io.awspring.cloud.v3.secretsmanager.AwsSecretsManagerPropertySources.AwsSecretsManagerPropertySourceNotFoundException;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+import software.amazon.awssdk.services.secretsmanager.model.ResourceNotFoundException;
+
+/**
+ * Unit test for {@link AwsSecretsManagerPropertySourceLocator}.
+ *
+ * @author Anthony Foulfoin
+ * @author Matej Nedic
+ * @author Maciej Walkowiak.
+ */
+class AwsSecretsManagerPropertySourceLocatorTest {
+
+	private final SecretsManagerClient smClient = mock(SecretsManagerClient.class);
+
+	private MockEnvironment env = new MockEnvironment();
+
+	@Test
+	void locate_nameSpecifiedInConstructor_returnsPropertySourceWithSpecifiedName() {
+
+		GetSecretValueResponse secretValueResult = GetSecretValueResponse.builder()
+				.secretString("{\"key1\": \"value1\", \"key2\": \"value2\"}").build();
+		when(smClient.getSecretValue(any(GetSecretValueRequest.class))).thenReturn(secretValueResult);
+
+		AwsSecretsManagerProperties properties = new AwsSecretsManagerProperties();
+		AwsSecretsManagerPropertySourceLocator locator = new AwsSecretsManagerPropertySourceLocator("my-name", smClient,
+				properties);
+
+		PropertySource propertySource = locator.locate(env);
+
+		assertThat(propertySource.getName()).isEqualTo("my-name");
+	}
+
+	@Test
+	void locate_nameNotSpecifiedInConstructor_returnsPropertySourceWithDefaultName() {
+		GetSecretValueResponse secretValueResult = GetSecretValueResponse.builder()
+				.secretString("{\"key1\": \"value1\", \"key2\": \"value2\"}").build();
+
+		when(smClient.getSecretValue(any(GetSecretValueRequest.class))).thenReturn(secretValueResult);
+
+		AwsSecretsManagerProperties properties = new AwsSecretsManagerProperties();
+		AwsSecretsManagerPropertySourceLocator locator = new AwsSecretsManagerPropertySourceLocator(smClient,
+				properties);
+		PropertySource propertySource = locator.locate(env);
+
+		assertThat(propertySource.getName()).isEqualTo("aws-secrets-manager");
+	}
+
+	@Test
+	void contextExpectedToHave2Elements() {
+		AwsSecretsManagerProperties properties = new AwsSecretsManagerPropertiesBuilder()
+				.withDefaultContext("application").withName("application").build();
+
+		GetSecretValueResponse secretValueResult = GetSecretValueResponse.builder()
+				.secretString("{\"key1\": \"value1\", \"key2\": \"value2\"}").build();
+
+		when(smClient.getSecretValue(any(GetSecretValueRequest.class))).thenReturn(secretValueResult);
+
+		AwsSecretsManagerPropertySourceLocator locator = new AwsSecretsManagerPropertySourceLocator(smClient,
+				properties);
+		env.setActiveProfiles("test");
+		locator.locate(env);
+
+		assertThat(locator.getContexts()).hasSize(2);
+	}
+
+	@Test
+	void contextExpectedToHave4Elements() {
+		AwsSecretsManagerProperties properties = new AwsSecretsManagerPropertiesBuilder()
+				.withDefaultContext("application").withName("messaging-service").build();
+
+		GetSecretValueResponse secretValueResult = GetSecretValueResponse.builder()
+				.secretString("{\"key1\": \"value1\", \"key2\": \"value2\"}").build();
+		when(smClient.getSecretValue(any(GetSecretValueRequest.class))).thenReturn(secretValueResult);
+
+		AwsSecretsManagerPropertySourceLocator locator = new AwsSecretsManagerPropertySourceLocator(smClient,
+				properties);
+		env.setActiveProfiles("test");
+		locator.locate(env);
+
+		assertThat(locator.getContexts()).hasSize(4);
+	}
+
+	@Test
+	void contextSpecificOrderExpected() {
+		AwsSecretsManagerProperties properties = new AwsSecretsManagerPropertiesBuilder()
+				.withDefaultContext("application").withName("messaging-service").build();
+
+		GetSecretValueResponse secretValueResult = GetSecretValueResponse.builder()
+				.secretString("{\"key1\": \"value1\", \"key2\": \"value2\"}").build();
+		when(smClient.getSecretValue(any(GetSecretValueRequest.class))).thenReturn(secretValueResult);
+
+		AwsSecretsManagerPropertySourceLocator locator = new AwsSecretsManagerPropertySourceLocator(smClient,
+				properties);
+		env.setActiveProfiles("test");
+		locator.locate(env);
+
+		List<String> contextToBeTested = new ArrayList<>(locator.getContexts());
+
+		assertThat(contextToBeTested.get(0)).isEqualTo("/secret/messaging-service_test");
+		assertThat(contextToBeTested.get(1)).isEqualTo("/secret/messaging-service");
+		assertThat(contextToBeTested.get(2)).isEqualTo("/secret/application_test");
+		assertThat(contextToBeTested.get(3)).isEqualTo("/secret/application");
+	}
+
+	@Test
+	void whenFailFastIsTrueAndSecretDoesNotExistThrowsException() {
+		AwsSecretsManagerProperties properties = new AwsSecretsManagerProperties();
+		properties.setFailFast(true);
+
+		when(smClient.getSecretValue(any(GetSecretValueRequest.class))).thenThrow(ResourceNotFoundException.class);
+
+		AwsSecretsManagerPropertySourceLocator locator = new AwsSecretsManagerPropertySourceLocator(smClient,
+				properties);
+		assertThatThrownBy(() -> locator.locate(env))
+				.isInstanceOf(AwsSecretsManagerPropertySourceNotFoundException.class);
+	}
+
+	@Test
+	void whenFailFastIsFalseAndSecretDoesNotExistReturnsEmptyPropertySource() {
+		AwsSecretsManagerProperties properties = new AwsSecretsManagerProperties();
+		properties.setFailFast(false);
+
+		when(smClient.getSecretValue(any(GetSecretValueRequest.class))).thenThrow(ResourceNotFoundException.class);
+
+		AwsSecretsManagerPropertySourceLocator locator = new AwsSecretsManagerPropertySourceLocator(smClient,
+				properties);
+
+		CompositePropertySource result = (CompositePropertySource) locator.locate(env);
+
+		assertThat(result.getPropertySources()).isEmpty();
+	}
+
+	private final static class AwsSecretsManagerPropertiesBuilder {
+
+		private final AwsSecretsManagerProperties properties = new AwsSecretsManagerProperties();
+
+		private AwsSecretsManagerPropertiesBuilder() {
+		}
+
+		public AwsSecretsManagerPropertiesBuilder withDefaultContext(String defaultContext) {
+			this.properties.setDefaultContext(defaultContext);
+			return this;
+		}
+
+		public AwsSecretsManagerPropertiesBuilder withName(String name) {
+			this.properties.setName(name);
+			return this;
+		}
+
+		public AwsSecretsManagerProperties build() {
+			return this.properties;
+		}
+
+	}
+
+}
