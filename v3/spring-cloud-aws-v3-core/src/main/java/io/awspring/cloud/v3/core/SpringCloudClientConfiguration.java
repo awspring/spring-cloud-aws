fb757466cad31b42484590ca308ec503a7ef9a67
Arun Patra <arunkpatra@gmail.com>
2021-02-19 21:15:35 +0530

    Migrated Secrets Manager to use AWS SK V2. Secrets are now loaded in config via the spring.config.import approach. Support for legacy way of loading properties has been dropped.

---
+++ v3/spring-cloud-aws-v3-core/src/main/java/io/awspring/cloud/v3/core/SpringCloudClientConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.awspring.cloud.v3.core;
+
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.client.config.SdkAdvancedClientOption;
+
+/**
+ * Provides configuration related overrides that may be applied across various AWS service
+ * integrations.
+ *
+ * @author Eddú Meléndez
+ * @author Arun Patra
+ */
+public final class SpringCloudClientConfiguration {
+
+	private static final String NAME = "spring-cloud-aws";
+
+	private static final String VERSION = "3.0.0";
+
+	private SpringCloudClientConfiguration() {
+
+	}
+
+	private static String getUserAgent() {
+		return NAME + "/" + VERSION;
+	}
+
+	/**
+	 * Returns desired client config overrides.
+	 * @return the client configuration overrides.
+	 */
+	public static ClientOverrideConfiguration clientOverrideConfiguration() {
+		return ClientOverrideConfiguration.builder()
+				.putAdvancedOption(SdkAdvancedClientOption.USER_AGENT_SUFFIX, getUserAgent()).build();
+	}
+
+}
