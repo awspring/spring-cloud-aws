a4a1534586def4644607927e9bc30f0399888770
Arun Patra <arunkpatra@gmail.com>
2021-02-19 21:15:35 +0530

    Migrated Secrets Manager to use AWS SK V2. Secrets are now loaded in config via the spring.config.import approach. Support for legacy way of loading properties has been dropped.

---
+++ v3/spring-cloud-aws-v3-secrets-manager-config/src/test/java/io/awspring/cloud/v3/secretsmanager/AwsSecretsManagerPropertySourceTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.awspring.cloud.v3.secretsmanager;
+
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+import software.amazon.awssdk.services.secretsmanager.model.ResourceNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link AwsSecretsManagerPropertySource}.
+ *
+ */
+
+class AwsSecretsManagerPropertySourceTest {
+
+	private SecretsManagerClient client = mock(SecretsManagerClient.class);
+
+	private AwsSecretsManagerPropertySource propertySource = new AwsSecretsManagerPropertySource("/config/myservice",
+			client);
+
+	@Test
+	void shouldParseSecretValue() {
+		GetSecretValueResponse secretValueResult = GetSecretValueResponse.builder()
+				.secretString("{\"key1\": \"value1\", \"key2\": \"value2\"}").build();
+
+		when(client.getSecretValue(any(GetSecretValueRequest.class))).thenReturn(secretValueResult);
+
+		propertySource.init();
+
+		assertThat(propertySource.getPropertyNames()).containsExactly("key1", "key2");
+		assertThat(propertySource.getProperty("key1")).isEqualTo("value1");
+		assertThat(propertySource.getProperty("key2")).isEqualTo("value2");
+	}
+
+	@Test
+	void throwsExceptionWhenSecretNotFound() {
+		when(client.getSecretValue(any(GetSecretValueRequest.class)))
+				.thenThrow(ResourceNotFoundException.builder().message("secret not found").build());
+
+		assertThatThrownBy(() -> propertySource.init()).isInstanceOf(ResourceNotFoundException.class);
+	}
+
+}
