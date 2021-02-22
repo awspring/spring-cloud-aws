50a055841f478f07705b8e018596f91d340de93d
Arun Patra <arunkpatra@gmail.com>
2021-02-22 14:34:59 +0530

    Add SES AWS SDK V2 integration
    See gh-84

---
+++ v3/spring-cloud-aws-v3-autoconfigure/src/main/java/io/awspring/cloud/v3/autoconfigure/ses/SesAutoConfiguration.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2013-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.awspring.cloud.v3.autoconfigure.ses;
+
+import javax.mail.Session;
+
+import io.awspring.cloud.v3.autoconfigure.CredentialsProviderAutoConfiguration;
+import io.awspring.cloud.v3.autoconfigure.RegionProviderAutoConfiguration;
+import io.awspring.cloud.v3.ses.SimpleEmailServiceJavaMailSender;
+import io.awspring.cloud.v3.ses.SimpleEmailServiceMailSender;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.regions.providers.AwsRegionProvider;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.SesClientBuilder;
+import software.amazon.awssdk.utils.StringUtils;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+
+/**
+ * {@link EnableAutoConfiguration} for {@link SimpleEmailServiceMailSender} and
+ * {@link SimpleEmailServiceJavaMailSender}.
+ *
+ * @author Agim Emruli
+ * @author Eddú Meléndez
+ * @author Arun Patra
+ */
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(SesProperties.class)
+@AutoConfigureAfter({ CredentialsProviderAutoConfiguration.class, RegionProviderAutoConfiguration.class })
+@ConditionalOnProperty(name = "spring.cloud.aws.ses.enabled", havingValue = "true", matchIfMissing = true)
+public class SesAutoConfiguration {
+
+	private final SesProperties properties;
+
+	public SesAutoConfiguration(SesProperties properties) {
+		this.properties = properties;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public SesClient sesClient(AwsCredentialsProvider awsCredentialsProvider, AwsRegionProvider awsRegionProvider) {
+		SesClientBuilder sesV2ClientBuilder = SesClient.builder().credentialsProvider(awsCredentialsProvider);
+
+		if (!StringUtils.isEmpty(properties.getRegion())) {
+			sesV2ClientBuilder.region(Region.of(properties.getRegion()));
+		}
+		else {
+			sesV2ClientBuilder.region(awsRegionProvider.getRegion());
+		}
+		return sesV2ClientBuilder.build();
+	}
+
+	@Bean
+	@ConditionalOnMissingClass("javax.mail.Session")
+	public MailSender simpleMailSender(SesClient sesClient) {
+		return new SimpleEmailServiceMailSender(sesClient);
+	}
+
+	@Bean
+	@ConditionalOnClass(Session.class)
+	public JavaMailSender javaMailSender(SesClient sesClient) {
+		return new SimpleEmailServiceJavaMailSender(sesClient);
+	}
+
+}
---
+++ v3/spring-cloud-aws-v3-ses/src/main/java/io/awspring/cloud/v3/ses/SimpleEmailServiceMailSender.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2013-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.awspring.cloud.v3.ses;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.Body;
+import software.amazon.awssdk.services.ses.model.Content;
+import software.amazon.awssdk.services.ses.model.Destination;
+import software.amazon.awssdk.services.ses.model.Message;
+import software.amazon.awssdk.services.ses.model.SendEmailRequest;
+import software.amazon.awssdk.services.ses.model.SendEmailResponse;
+import software.amazon.awssdk.services.ses.model.SesException;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.util.StringUtils;
+
+/**
+ * Simple MailSender implementation to send E-Mails with the Amazon Simple Email Service.
+ * This implementation has no dependencies to the Java Mail API. It can be used to send
+ * simple mail messages that doesn't have any attachment and therefore only consist of a
+ * text body and a subject line.
+ *
+ * @author Agim Emruli
+ * @author Eddú Meléndez
+ * @author Arun Patra
+ */
+public class SimpleEmailServiceMailSender implements MailSender, DisposableBean {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleEmailServiceMailSender.class);
+
+	private final SesClient sesClient;
+
+	public SimpleEmailServiceMailSender(SesClient sesClient) {
+		this.sesClient = sesClient;
+	}
+
+	@Override
+	public void destroy() {
+		sesClient.close();
+	}
+
+	@Override
+	public void send(SimpleMailMessage simpleMessage) throws MailException {
+		send(new SimpleMailMessage[] { simpleMessage });
+	}
+
+	@Override
+	public void send(SimpleMailMessage... simpleMessages) throws MailException {
+		Map<Object, Exception> failedMessages = new HashMap<>();
+
+		for (SimpleMailMessage simpleMessage : simpleMessages) {
+			try {
+				SendEmailResponse sendEmailResult = getEmailService().sendEmail(prepareMessage(simpleMessage));
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("Message with id: {} successfully send", sendEmailResult.messageId());
+				}
+			}
+			catch (SesException e) {
+				// Ignore Exception because we are collecting and throwing all if any
+				// noinspection ThrowableResultOfMethodCallIgnored
+				failedMessages.put(simpleMessage, e);
+			}
+		}
+
+		if (!failedMessages.isEmpty()) {
+			throw new MailSendException(failedMessages);
+		}
+
+	}
+
+	protected SesClient getEmailService() {
+		return this.sesClient;
+	}
+
+	private SendEmailRequest prepareMessage(SimpleMailMessage simpleMailMessage) {
+		Destination.Builder destinationBuilder = Destination.builder();
+		destinationBuilder.toAddresses(simpleMailMessage.getTo());
+
+		if (simpleMailMessage.getCc() != null) {
+			destinationBuilder.ccAddresses(simpleMailMessage.getCc());
+		}
+
+		if (simpleMailMessage.getBcc() != null) {
+			destinationBuilder.bccAddresses(simpleMailMessage.getBcc());
+		}
+
+		Content subject = Content.builder().data(simpleMailMessage.getSubject()).build();
+		Body body = Body.builder().text(Content.builder().data(simpleMailMessage.getText()).build()).build();
+		Message message = Message.builder().body(body).subject(subject).build();
+
+		SendEmailRequest.Builder emailRequestBuilder = SendEmailRequest.builder()
+				.destination(destinationBuilder.build()).source(simpleMailMessage.getFrom()).message(message);
+
+		// .content(EmailContent.builder().simple(message).build());
+
+		if (StringUtils.hasText(simpleMailMessage.getReplyTo())) {
+			emailRequestBuilder.replyToAddresses(simpleMailMessage.getReplyTo());
+		}
+
+		return emailRequestBuilder.build();
+	}
+
+}
