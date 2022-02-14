9e16012e0041205ed84c012231852dc42a428e9f
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-02-14 13:59:23 +0100

    Update CDK, change stack name.

--- spring-cloud-aws-samples/spring-cloud-aws-sns-sample/infrastracture/bin/infrastructure.ts
+++ spring-cloud-aws-samples/spring-cloud-aws-sns-sample/infrastracture/bin/infrastructure.ts
@@ -4,4 +4,4 @@ import * as cdk from '@aws-cdk/core';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack');
+new InfrastructureStack(app, 'SnsSampleAppStack');
