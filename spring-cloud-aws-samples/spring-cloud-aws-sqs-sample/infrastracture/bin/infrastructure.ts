e61268cad7f4db7bf77d0636303b654bf072a074
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-02-22 10:34:26 +0100

    remove old modules, move v3 to root

--- spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/infrastracture/bin/infrastructure.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { InfrastructureStack } from '../lib/infrastructure-stack';
-
-const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack');
