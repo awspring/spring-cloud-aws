615b35a04e8a1032b2b7584678752f49406f9d73
Francesco Macagno <francesco@foursquare.com>
2022-08-21 01:13:05 -0500

    Read bucket region from redirect header (#453)
    Attempt to read bucket region from a redirect header in `CrossRegionS3Client` to avoid extra call to AWS.
    
    Fixes #452.
    
    Co-authored-by: Maciej Walkowiak <walkowiak.maciej@yahoo.com>

--- spring-cloud-aws-s3-parent/spring-cloud-aws-s3-codegen/src/main/java/io/awspring/cloud/s3/codegen/AbstractCrossRegionS3ClientGenerator.java
+++ spring-cloud-aws-s3-parent/spring-cloud-aws-s3-codegen/src/main/java/io/awspring/cloud/s3/codegen/AbstractCrossRegionS3ClientGenerator.java
@@ -20,7 +20,6 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.resolution.MethodUsage;
 import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
@@ -30,75 +29,72 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.services.s3.S3Client;
 
 /**
- * Generates CrossRegionS3Client class from {@link CrossRegionS3ClientTemplate}.
+ * Generates AbstractCrossRegionS3Client class from {@link AbstractCrossRegionS3ClientTemplate}.
  *
  * Generated methods wrap every bucket-specific {@link S3Client} method with
- * {@link CrossRegionS3ClientTemplate#executeInBucketRegion(String, Function)}.
+ * {@link AbstractCrossRegionS3ClientTemplate#executeInBucketRegion(String, Function)} and every bucket-independent
+ * {@link S3Client} method with {@link AbstractCrossRegionS3ClientTemplate#executeInDefaultRegion(Function)}
  *
  * @author Maciej Walkowiak
  * @since 3.0
  */
-public final class CrossRegionS3ClientGenerator {
+public final class AbstractCrossRegionS3ClientGenerator {
 
-	private CrossRegionS3ClientGenerator() {
+	private AbstractCrossRegionS3ClientGenerator() {
 
 	}
 
 	public static void main(String[] args) throws IOException {
 		if (args.length != 1) {
-			throw new RuntimeException("Need 1 parameter: the JavaParser source checkout root directory.");
+			throw new IllegalArgumentException("Need 1 parameter: the JavaParser source checkout root directory.");
 		}
 
 		// load template class
 		final Path source = Paths.get(args[0], "..", "spring-cloud-aws-s3-codegen", "src", "main", "java", "io",
-				"awspring", "cloud", "s3", "codegen", "CrossRegionS3ClientTemplate.java");
+				"awspring", "cloud", "s3", "codegen", "AbstractCrossRegionS3ClientTemplate.java");
 		CompilationUnit compilationUnit = StaticJavaParser.parse(source);
 		compilationUnit.setPackageDeclaration("io.awspring.cloud.s3.crossregion");
 		ClassOrInterfaceDeclaration classOrInterfaceDeclaration = compilationUnit
-				.getClassByName("CrossRegionS3ClientTemplate")
-				.orElseThrow(() -> new IllegalStateException("Class CrossRegionS3ClientTemplate not found"));
+				.getClassByName("AbstractCrossRegionS3ClientTemplate")
+				.orElseThrow(() -> new IllegalStateException("Class AbstractCrossRegionS3ClientTemplate not found"));
 
 		// rename class and constructors
-		classOrInterfaceDeclaration.setName("CrossRegionS3Client");
+		classOrInterfaceDeclaration.setName("AbstractCrossRegionS3Client");
 		classOrInterfaceDeclaration.getConstructors()
-				.forEach(constructorDeclaration -> constructorDeclaration.setName("CrossRegionS3Client"));
+				.forEach(constructorDeclaration -> constructorDeclaration.setName("AbstractCrossRegionS3Client"));
 
 		// add methods
 		addOverriddenMethods(classOrInterfaceDeclaration);
 
 		// generate target file
 		final Path generatedJavaCcRoot = Paths.get(args[0], "..", "spring-cloud-aws-s3-cross-region-client", "src",
-				"main", "java", "io", "awspring", "cloud", "s3", "crossregion", "CrossRegionS3Client.java");
+				"main", "java", "io", "awspring", "cloud", "s3", "crossregion", "AbstractCrossRegionS3Client.java");
 		Files.write(generatedJavaCcRoot, Collections.singletonList(compilationUnit.toString()));
 	}
 
 	private static void addOverriddenMethods(ClassOrInterfaceDeclaration crossRegionS3Client) {
-		TypeSolver typeSolver = new ClassLoaderTypeSolver(CrossRegionS3ClientGenerator.class.getClassLoader());
+		TypeSolver typeSolver = new ClassLoaderTypeSolver(AbstractCrossRegionS3ClientGenerator.class.getClassLoader());
 		ResolvedReferenceTypeDeclaration resolvedReferenceTypeDeclaration = typeSolver
 				.solveType(S3Client.class.getName());
 		resolvedReferenceTypeDeclaration.getAllMethods().stream()
 				.sorted(Comparator.comparing(MethodUsage::getSignature)).forEach(method -> {
-					if (isRegionSpecific(method) && isCanonical(method)) {
+					if (isCanonical(method)) {
 						MethodDeclaration methodDeclaration = crossRegionS3Client.addMethod(method.getName(),
 								Modifier.Keyword.PUBLIC);
 
 						// collect & create method parameters
-						List<String> parameterStrings = new ArrayList<>();
 						for (int i = 0; i < method.getParamTypes().size(); i++) {
-							methodDeclaration.addParameter(new Parameter(StaticJavaParser
-									.parseClassOrInterfaceType(method.getParamTypes().get(i).describe()), "p" + i));
-							parameterStrings.add("p" + i);
+							methodDeclaration.addParameter(StaticJavaParser
+									.parseClassOrInterfaceType(method.getParamTypes().get(i).describe()), "p" + i);
 						}
 
 						// handle generic types
@@ -108,9 +104,7 @@ public final class CrossRegionS3ClientGenerator {
 
 						methodDeclaration.setType(StaticJavaParser
 								.parseClassOrInterfaceType(method.getDeclaration().getReturnType().describe()));
-						methodDeclaration.setBody(new BlockStmt()
-								.addStatement("return executeInBucketRegion(p0.bucket(), s3Client -> s3Client."
-										+ method.getName() + "(" + String.join(",", parameterStrings) + "));"));
+						methodDeclaration.setBody(getMethodBody(method, methodDeclaration));
 						methodDeclaration.addMarkerAnnotation(Override.class);
 						methodDeclaration.addThrownException(AwsServiceException.class);
 						methodDeclaration.addThrownException(SdkClientException.class);
@@ -118,12 +112,28 @@ public final class CrossRegionS3ClientGenerator {
 				});
 	}
 
+	private static BlockStmt getMethodBody(MethodUsage method, MethodDeclaration methodDeclaration) {
+		BlockStmt body = new BlockStmt();
+		String params = methodDeclaration.getParameters().stream().map(p -> p.getName().asString())
+				.collect(Collectors.joining(","));
+		if (isRegionSpecific(method)) {
+			body.addStatement("return executeInBucketRegion(" + methodDeclaration.getParameter(0).getName().asString()
+					+ ".bucket(), s3Client -> s3Client." + method.getName() + "(" + params + "));");
+		}
+		else {
+			body.addStatement(
+					"return executeInDefaultRegion(s3Client -> s3Client." + method.getName() + "(" + params + "));");
+		}
+		return body;
+	}
+
 	private static boolean isRegionSpecific(MethodUsage method) {
-		return !Arrays.asList("listBuckets", "writeGetObjectResponse").contains(method.getName());
+		return method.getDeclaration().getParam(0).getType().asReferenceType().getDeclaredMethods().stream()
+				.anyMatch(mu -> mu.getName().equals("bucket"));
 	}
 
 	private static boolean isCanonical(MethodUsage method) {
-		return method.getParamTypes().size() > 0 && method.getParamType(0).describe().endsWith("Request");
+		return !method.getParamTypes().isEmpty() && method.getParamType(0).describe().endsWith("Request");
 	}
 
 }
