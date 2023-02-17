1d2f0b6582dbe9bfdded011bfbd7e55e37d055ff
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2023-02-17 17:34:55 +0100

    Remove "enabled" from Parameter Store and Secrets Manager docs.
    Fixes #671

--- spring-cloud-aws-autoconfigure/src/main/java/io/awspring/cloud/autoconfigure/config/parameterstore/ParameterStoreMissingKeysFailureAnalyzer.java
+++ spring-cloud-aws-autoconfigure/src/main/java/io/awspring/cloud/autoconfigure/config/parameterstore/ParameterStoreMissingKeysFailureAnalyzer.java
@@ -31,7 +31,7 @@ public class ParameterStoreMissingKeysFailureAnalyzer
 	@Override
 	protected FailureAnalysis analyze(Throwable rootFailure, ParameterStoreKeysMissingException cause) {
 		return new FailureAnalysis("Could not import properties from AWS Parameter Store: " + cause.getMessage(),
-				"Consider providing keys, for example `spring.config.import=aws-parameterstore:/config/app` or disable integration with `spring.cloud.aws.parameterstore.enabled=false",
+				"Consider providing keys, for example `spring.config.import=aws-parameterstore:/config/app`",
 				cause);
 	}
 
