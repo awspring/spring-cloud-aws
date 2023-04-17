de601ed86c1eca546121a436ea4007dec14d669d
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2023-04-17 12:02:06 +0200

    Downgrade Localstack to 1.4.0 (#766)
    Localstack 2.0 introduced flakiness to tests.

--- spring-cloud-aws-autoconfigure/src/test/java/io/awspring/cloud/autoconfigure/metrics/CloudWatchExportAutoConfigurationIntegrationTests.java
+++ spring-cloud-aws-autoconfigure/src/test/java/io/awspring/cloud/autoconfigure/metrics/CloudWatchExportAutoConfigurationIntegrationTests.java
@@ -55,7 +55,7 @@ class CloudWatchExportAutoConfigurationIntegrationTests {
 
 	@Container
 	static LocalStackContainer localstack = new LocalStackContainer(
-			DockerImageName.parse("localstack/localstack:2.0.0"));
+			DockerImageName.parse("localstack/localstack:1.4.0"));
 
 	@DynamicPropertySource
 	static void registerProperties(DynamicPropertyRegistry registry) {
