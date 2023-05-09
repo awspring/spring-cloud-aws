fd0a2e2f7b5c9154a64329339ad61661c2084704
Matheus Cruz <matheuscruz.dev@gmail.com>
2023-05-09 09:39:39 -0300

    refactor: remove main method and organize imports

--- spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/src/main/java/io/awspring/cloud/sqs/sample/SqsManualAckSample.java
+++ spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/src/main/java/io/awspring/cloud/sqs/sample/SqsManualAckSample.java
@@ -3,14 +3,11 @@ package io.awspring.cloud.sqs.sample;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
 import io.awspring.cloud.sqs.listener.acknowledgement.Acknowledgement;
-import io.awspring.cloud.sqs.listener.acknowledgement.AcknowledgementOrdering;
 import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
-import io.awspring.cloud.sqs.operations.TemplateAcknowledgementMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
@@ -22,48 +19,44 @@ import java.util.UUID;
 @Configuration
 public class SqsManualAckSample {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SqsManualAckSample.class);
-    }
+	public static final String NEW_USER_QUEUE = "new-user-queue";
 
-    public static final String NEW_USER_QUEUE = "new-user-queue";
+	private static final Logger LOGGER = LoggerFactory.getLogger(SqsManualAckSample.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SqsManualAckSample.class);
+	@Bean
+	public ApplicationRunner sendMessageToQueue(SqsTemplate sqsTemplate) {
+		LOGGER.info("Sending message");
+		return args -> sqsTemplate.send(to -> to.queue(NEW_USER_QUEUE)
+			.payload(new User(UUID.randomUUID(), "John"))
+		);
+	}
 
-    @Bean
-    public ApplicationRunner sendMessageToQueue(SqsTemplate sqsTemplate) {
-        LOGGER.info("Sending message");
-        return args -> sqsTemplate.send(to -> to.queue(NEW_USER_QUEUE)
-                .payload(new User(UUID.randomUUID(), "John"))
-        );
-    }
+	@Bean
+	public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient) {
+		return SqsTemplate.builder()
+			.sqsAsyncClient(sqsAsyncClient)
+			.build();
+	}
 
-    @Bean
-    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient) {
-        return SqsTemplate.builder()
-                .sqsAsyncClient(sqsAsyncClient)
-                .build();
-    }
-
-    @SqsListener(NEW_USER_QUEUE)
-    public void listen(Message<User> message) {
-        Acknowledgement.acknowledge(message);
+	@SqsListener(NEW_USER_QUEUE)
+	public void listen(Message<User> message) {
+		Acknowledgement.acknowledge(message);
 		LOGGER.info("Message {} acknowledged", message.getPayload());
-    }
+	}
 
-    @Bean
-    SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
-        return SqsMessageListenerContainerFactory
-                .builder()
-                .configure(options -> options
-                        .acknowledgementMode(AcknowledgementMode.MANUAL)
-                        .acknowledgementInterval(Duration.ZERO) // Set to Duration.ZERO along with
-                        .acknowledgementThreshold(0) 			// acknowledgementThreshold to zero to enable immediate acknowledgement.
-                )
-                .sqsAsyncClient(sqsAsyncClient)
-                .build();
-    }
+	@Bean
+	SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
+		return SqsMessageListenerContainerFactory
+			.builder()
+			.configure(options -> options
+				.acknowledgementMode(AcknowledgementMode.MANUAL)
+				.acknowledgementInterval(Duration.ZERO) // Set to Duration.ZERO along with
+				.acknowledgementThreshold(0)            // acknowledgementThreshold to zero to enable immediate acknowledgement.
+			)
+			.sqsAsyncClient(sqsAsyncClient)
+			.build();
+	}
 
-    public record User(UUID id, String name) {
-    }
+	public record User(UUID id, String name) {
+	}
 }
