e2998650af71771ea5f8947fd76aa248d3320800
Matheus Cruz <matheuscruz.dev@gmail.com>
2023-06-03 00:30:25 -0300

    refactor: apply pr suggestions

--- spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/src/main/java/io/awspring/cloud/sqs/sample/SqsManualContainerInstantiationSample.java
+++ spring-cloud-aws-samples/spring-cloud-aws-sqs-sample/src/main/java/io/awspring/cloud/sqs/sample/SqsManualContainerInstantiationSample.java
@@ -16,26 +16,19 @@
 package io.awspring.cloud.sqs.sample;
 
 import io.awspring.cloud.sqs.listener.SqsMessageListenerContainer;
-import io.awspring.cloud.sqs.listener.acknowledgement.Acknowledgement;
-import io.awspring.cloud.sqs.listener.acknowledgement.AcknowledgementResultCallback;
-import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
-import io.awspring.cloud.sqs.operations.TemplateAcknowledgementMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
-import java.util.Collection;
 import java.util.UUID;
 
 @Configuration
 public class SqsManualContainerInstantiationSample {
-
-
+	
 	public static final String NEW_USER_QUEUE = "new-user-queue";
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SqsManualContainerInstantiationSample.class);
@@ -52,37 +45,21 @@ public class SqsManualContainerInstantiationSample {
 	public SqsTemplate sqsTemplateManualContainerInstantiation(SqsAsyncClient sqsAsyncClient) {
 		return SqsTemplate.builder()
 			.sqsAsyncClient(sqsAsyncClient)
-			.configure(options -> options.acknowledgementMode(TemplateAcknowledgementMode.MANUAL))
 			.build();
 	}
 
 	@Bean
-	SqsMessageListenerContainer<User> sqsMessageListenerContainer(SqsAsyncClient sqsAsyncClient) {
-		SqsMessageListenerContainer<User> container = new SqsMessageListenerContainer<>(sqsAsyncClient);
-		container.setMessageListener((message) -> {
-			LOGGER.info("Received message {}", message);
-			Acknowledgement.acknowledge(message);
-		});
-		container.setQueueNames(NEW_USER_QUEUE);
-		container.setAcknowledgementResultCallback(new AckResultCallback());
-		container.configure(sqsContainerOptionsBuilder ->
-			sqsContainerOptionsBuilder
-				.acknowledgementMode(AcknowledgementMode.MANUAL));
-		return container;
-	}
-
-	static class AckResultCallback implements AcknowledgementResultCallback<User> {
-		@Override
-		public void onSuccess(Collection<Message<User>> messages) {
-			LOGGER.info("Ack with success");
-		}
-
-		@Override
-		public void onFailure(Collection<Message<User>> messages, Throwable t) {
-			LOGGER.error("Ack with fail", t);
-		}
+	SqsMessageListenerContainer<Object> sqsMessageListenerContainer(SqsAsyncClient sqsAsyncClient) {
+		return SqsMessageListenerContainer.builder()
+			.sqsAsyncClient(sqsAsyncClient)
+			.queueNames(NEW_USER_QUEUE)
+			.messageListener((message) -> {
+				LOGGER.info("Received message {}", message);
+			})
+			.build();
 	}
 
 	public record User(UUID id, String name) {
 	}
 }
+
