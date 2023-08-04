ec900a03edd03e59dc05426cd5a60d06b79804f4
matejnedic <matejnedic1@gmail.com>
2023-08-04 17:12:29 +0200

    Introduce ListenerReflectiveProcessor to SqsListener

--- spring-cloud-aws-sqs/src/main/java/io/awspring/cloud/sqs/annotation/SqsListenerReflectiveProcessor.java
+++ spring-cloud-aws-sqs/src/main/java/io/awspring/cloud/sqs/annotation/SqsListenerReflectiveProcessor.java
@@ -1,15 +1,29 @@
+/*
+ * Copyright 2013-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.awspring.cloud.sqs.annotation;
-
-import org.springframework.aot.hint.BindingReflectionHintsRegistrar;
-import org.springframework.aot.hint.ExecutableMode;
-import org.springframework.aot.hint.ReflectionHints;
-import org.springframework.aot.hint.annotation.ReflectiveProcessor;
-import org.springframework.core.MethodParameter;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.lang.reflect.Type;
+import org.springframework.aot.hint.BindingReflectionHintsRegistrar;
+import org.springframework.aot.hint.ExecutableMode;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.annotation.ReflectiveProcessor;
+import org.springframework.core.MethodParameter;
 
 /**
  * Heavily inspired by Spring Frameworks ControllerMappingReflectiveProcessor.
@@ -56,6 +70,6 @@ public class SqsListenerReflectiveProcessor implements ReflectiveProcessor {
 	private Type getEntityType(MethodParameter parameter) {
 		MethodParameter nestedParameter = parameter.nested();
 		return (nestedParameter.getNestedParameterType() == nestedParameter.getParameterType() ? null
-			: nestedParameter.getNestedParameterType());
+				: nestedParameter.getNestedParameterType());
 	}
 }
