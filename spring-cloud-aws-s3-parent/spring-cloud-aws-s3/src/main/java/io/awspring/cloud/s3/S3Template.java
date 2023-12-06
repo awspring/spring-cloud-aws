4f8f480ceae8153d5226481c0487ac522c0a2be2
Ziemowit Stolarczyk <ziemowit.stolarczyk@gmail.com>
2023-12-06 23:38:00 +0100

    Add S3Client objectExists support in S3Template

--- spring-cloud-aws-s3-parent/spring-cloud-aws-s3/src/main/java/io/awspring/cloud/s3/S3Template.java
+++ spring-cloud-aws-s3-parent/spring-cloud-aws-s3/src/main/java/io/awspring/cloud/s3/S3Template.java
@@ -28,6 +28,7 @@ import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
@@ -39,6 +40,7 @@ import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignReques
  * Higher level abstraction over {@link S3Client} providing methods for the most common use cases.
  *
  * @author Maciej Walkowiak
+ * @author Ziemowit Stolarczyk
  * @since 3.0
  */
 public class S3Template implements S3Operations {
@@ -99,6 +101,19 @@ public class S3Template implements S3Operations {
 		Assert.notNull(s3Url, "s3Url is required");
 		Location location = Location.of(s3Url);
 		this.deleteObject(location.getBucket(), location.getObject());
+	}
+
+	@Override
+	public boolean objectExists(String bucketName, String key) {
+		Assert.notNull(bucketName, "bucketName is required");
+		Assert.notNull(key, "key is required");
+		try {
+			s3Client.headObject(request -> request.bucket(bucketName).key(key));
+		}
+		catch (NoSuchBucketException | NoSuchKeyException e) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
