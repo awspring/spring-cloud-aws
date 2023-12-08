8345b481fa71e4f3df60f6f30b1b7f7316d737a9
Ziemowit <Ziemowit.Stolarczyk@gmail.com>
2023-12-08 23:42:58 +0100

    Add S3Client objectExists support in S3Template (#978)

--- spring-cloud-aws-s3-parent/spring-cloud-aws-s3/src/main/java/io/awspring/cloud/s3/S3Operations.java
+++ spring-cloud-aws-s3-parent/spring-cloud-aws-s3/src/main/java/io/awspring/cloud/s3/S3Operations.java
@@ -28,6 +28,7 @@ import software.amazon.awssdk.services.s3.model.CreateBucketResponse;
  * {@link S3Template}.
  *
  * @author Maciej Walkowiak
+ * @author Ziemowit Stolarczyk
  * @since 3.0
  */
 public interface S3Operations {
@@ -69,6 +70,15 @@ public interface S3Operations {
 	 * @param s3Url - the S3 url s3://bucket/key
 	 */
 	void deleteObject(String s3Url);
+
+	/**
+	 * Checks if an S3 object exists.
+	 *
+	 * @param bucketName - the bucket name
+	 * @param key - the object key
+	 * @return true if object exists; false otherwise
+	 */
+	boolean objectExists(String bucketName, String key);
 
 	/**
 	 * Returns some or all (up to 1,000) of the objects in a bucket. Does not handle pagination. If you need pagination
