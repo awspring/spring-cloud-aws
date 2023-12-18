2e0ac45e95549cd86cb0f1318e044464770c48bc
Cott Lang <cott@possiblefinance.com>
2023-12-18 14:12:12 -0800

    Assert queues are all the same type in setQueueNames so applications fail to start rather than throwing endless Executor errors.

--- spring-cloud-aws-sqs/src/main/java/io/awspring/cloud/sqs/listener/SqsMessageListenerContainer.java
+++ spring-cloud-aws-sqs/src/main/java/io/awspring/cloud/sqs/listener/SqsMessageListenerContainer.java
@@ -118,14 +118,21 @@ public class SqsMessageListenerContainer<T>
 
 	@Override
 	protected Collection<ContainerComponentFactory<T, SqsContainerOptions>> createDefaultComponentFactories() {
-		Assert.isTrue(allQueuesSameType(),
-				"SqsMessageListenerContainer must contain either all FIFO or all Standard queues.");
+		assertAllQueuesSameType();
 		return Arrays.asList(new FifoSqsComponentFactory<>(), new StandardSqsComponentFactory<>());
 	}
 
-	private boolean allQueuesSameType() {
-		return getQueueNames().stream().allMatch(this::isFifoQueue)
-				|| getQueueNames().stream().noneMatch(this::isFifoQueue);
+	@Override
+	public void setQueueNames(Collection<String> queueNames) {
+		super.setQueueNames(queueNames);
+		assertAllQueuesSameType();
+	}
+
+	private void assertAllQueuesSameType() {
+		Assert.isTrue(
+				getQueueNames().stream().allMatch(this::isFifoQueue)
+						|| getQueueNames().stream().noneMatch(this::isFifoQueue),
+				"SqsMessageListenerContainer must contain either all FIFO or all Standard queues.");
 	}
 
 	private boolean isFifoQueue(String name) {
