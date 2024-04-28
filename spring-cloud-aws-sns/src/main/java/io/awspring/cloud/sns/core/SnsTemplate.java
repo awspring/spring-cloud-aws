f59d4516f3571024408205b4ca2fcfbfe2459d70
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-04-28 13:32:30 +0530

    add boolean topicExists() method

--- spring-cloud-aws-sns/src/main/java/io/awspring/cloud/sns/core/SnsTemplate.java
+++ spring-cloud-aws-sns/src/main/java/io/awspring/cloud/sns/core/SnsTemplate.java
@@ -34,6 +34,7 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.util.Assert;
 import software.amazon.awssdk.arns.Arn;
 import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.NotFoundException;
 
 /**
  * Helper class that simplifies synchronous sending of notifications to SNS. The only mandatory fields are
@@ -150,6 +151,17 @@ public class SnsTemplate extends AbstractMessageSendingTemplate<TopicMessageChan
 	@Override
 	public void sendNotification(String topic, SnsNotification<?> notification) {
 		this.convertAndSend(topic, notification.getPayload(), notification.getHeaders());
+	}
+	
+	@Override
+	public boolean topicExists(String topicArn) {
+		Assert.notNull(topicArn, "topicArn must not be null");
+		try {
+			snsClient.getTopicAttributes(request -> request.topicArn(topicArn));
+		} catch (NotFoundException exception) {
+			return false;
+		}
+		return true;
 	}
 
 	private TopicMessageChannel resolveMessageChannelByTopicName(String topicName) {
