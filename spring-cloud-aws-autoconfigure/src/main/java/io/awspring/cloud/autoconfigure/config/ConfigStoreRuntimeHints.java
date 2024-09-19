b8d6e5029fda80bcd883863534252f5373eaca42
matejnedic <matejnedic1@gmail.com>
2024-09-19 16:02:50 +0200

    add new hints

--- spring-cloud-aws-autoconfigure/src/main/java/io/awspring/cloud/autoconfigure/config/ConfigStoreRuntimeHints.java
+++ spring-cloud-aws-autoconfigure/src/main/java/io/awspring/cloud/autoconfigure/config/ConfigStoreRuntimeHints.java
@@ -17,21 +17,36 @@ package io.awspring.cloud.autoconfigure.config;
 
 import io.awspring.cloud.autoconfigure.config.parameterstore.ParameterStorePropertySources;
 import io.awspring.cloud.autoconfigure.config.secretsmanager.SecretsManagerPropertySources;
+import io.awspring.cloud.parameterstore.ParameterStorePropertySource;
+import io.awspring.cloud.secretsmanager.SecretsManagerPropertySource;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
+import org.springframework.util.ClassUtils;
 
 public class ConfigStoreRuntimeHints implements RuntimeHintsRegistrar {
 
 	@Override
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-		hints.reflection().registerType(TypeReference.of(ParameterStorePropertySources.class),
+		if (ClassUtils.isPresent("io.awspring.cloud.parameterstore.ParameterStorePropertySource", classLoader)) {
+			hints.reflection().registerType(TypeReference.of(ParameterStorePropertySources.class),
 				hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-						MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.DECLARED_FIELDS));
+					MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.DECLARED_FIELDS));
+			hints.reflection().registerType(TypeReference.of(ParameterStorePropertySource.class),
+				hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+					MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.DECLARED_FIELDS));
+		}
 
-		hints.reflection().registerType(TypeReference.of(SecretsManagerPropertySources.class),
+		if (ClassUtils.isPresent("io.awspring.cloud.secretsmanager.SecretsManagerPropertySource", classLoader)) {
+			hints.reflection().registerType(TypeReference.of(SecretsManagerPropertySources.class),
 				hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-						MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.DECLARED_FIELDS));
+					MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.DECLARED_FIELDS));
+
+			hints.reflection().registerType(TypeReference.of(SecretsManagerPropertySource.class),
+				hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+					MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.DECLARED_FIELDS));
+		}
+
 	}
 }
