f2388d8544895b39deb7d61a4f7c1c4fb98887cb
Bruno Augusto Garcia <b.augusto1501@gmail.com>
2025-07-03 01:28:12 -0300

    Add `ExponentialBackoffErrorHandlerWithFullJitter`, `ExponentialBackoffErrorHandlerWithHalfJitter`, `LinearBackoffErrorHandler` to manage message visibility timeouts and implement exponential backoff logic for retries in SQS (#1314)

--- spring-cloud-aws-sqs/src/main/java/io/awspring/cloud/sqs/listener/errorhandler/ExponentialBackoffErrorHandlerWithFullJitter.java
+++ spring-cloud-aws-sqs/src/main/java/io/awspring/cloud/sqs/listener/errorhandler/ExponentialBackoffErrorHandlerWithFullJitter.java
@@ -19,14 +19,17 @@ import io.awspring.cloud.sqs.MessageHeaderUtils;
 import io.awspring.cloud.sqs.listener.BatchVisibility;
 import io.awspring.cloud.sqs.listener.Visibility;
 import java.util.Collection;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
 import org.springframework.util.Assert;
 
 /**
- * An implementation of an Exponential Backoff error handler for asynchronous message processing.
+ * An implementation of an Exponential Backoff with full jitter error handler for asynchronous message processing.
  *
  * <p>
  * This error handler sets the SQS message visibility timeout exponentially based on the number of received attempts
@@ -39,34 +42,36 @@ import org.springframework.util.Assert;
  * @author Bruno Garcia
  * @author Rafael Pavarini
  */
-
-public class ExponentialBackoffErrorHandler<T> implements AsyncErrorHandler<T> {
-	private static final Logger logger = LoggerFactory.getLogger(ExponentialBackoffErrorHandler.class);
+public class ExponentialBackoffErrorHandlerWithFullJitter<T> implements AsyncErrorHandler<T> {
+	private static final Logger logger = LoggerFactory.getLogger(ExponentialBackoffErrorHandlerWithFullJitter.class);
 
 	private final int initialVisibilityTimeoutSeconds;
 	private final double multiplier;
 	private final int maxVisibilityTimeoutSeconds;
+	private final Supplier<Random> randomSupplier;
 
-	private ExponentialBackoffErrorHandler(int initialVisibilityTimeoutSeconds, double multiplier,
-			int maxVisibilityTimeoutSeconds) {
+	private ExponentialBackoffErrorHandlerWithFullJitter(int initialVisibilityTimeoutSeconds, double multiplier,
+			int maxVisibilityTimeoutSeconds, Supplier<Random> randomSupplier) {
 		this.initialVisibilityTimeoutSeconds = initialVisibilityTimeoutSeconds;
 		this.multiplier = multiplier;
 		this.maxVisibilityTimeoutSeconds = maxVisibilityTimeoutSeconds;
+		this.randomSupplier = randomSupplier;
 	}
 
 	@Override
 	public CompletableFuture<Void> handle(Message<T> message, Throwable t) {
-		return applyExponentialBackoffVisibilityTimeout(message)
+		return applyExponentialBackoffVisibilityTimeoutWithFullJitter(message)
 				.thenCompose(theVoid -> CompletableFuture.failedFuture(t));
 	}
 
 	@Override
 	public CompletableFuture<Void> handle(Collection<Message<T>> messages, Throwable t) {
-		return applyExponentialBackoffVisibilityTimeout(messages)
+		return applyExponentialBackoffVisibilityTimeoutWithFullJitter(messages)
 				.thenCompose(theVoid -> CompletableFuture.failedFuture(t));
 	}
 
-	private CompletableFuture<Void> applyExponentialBackoffVisibilityTimeout(Collection<Message<T>> messages) {
+	private CompletableFuture<Void> applyExponentialBackoffVisibilityTimeoutWithFullJitter(
+			Collection<Message<T>> messages) {
 		CompletableFuture<?>[] futures = ErrorHandlerVisibilityHelper.groupMessagesByReceiveMessageCount(messages)
 				.entrySet().stream().map(entry -> {
 					int timeout = calculateTimeout(entry.getKey());
@@ -87,7 +92,7 @@ public class ExponentialBackoffErrorHandler<T> implements AsyncErrorHandler<T> {
 		});
 	}
 
-	private CompletableFuture<Void> applyExponentialBackoffVisibilityTimeout(Message<T> message) {
+	private CompletableFuture<Void> applyExponentialBackoffVisibilityTimeoutWithFullJitter(Message<T> message) {
 		int timeout = calculateTimeout(message);
 		Visibility visibility = ErrorHandlerVisibilityHelper.getVisibility(message);
 		logger.debug("Changing visibility timeout to {} - Message Id {}", timeout, message.getHeaders().getId());
@@ -104,12 +109,13 @@ public class ExponentialBackoffErrorHandler<T> implements AsyncErrorHandler<T> {
 	}
 
 	private int calculateTimeout(long receiveMessageCount) {
-		return ErrorHandlerVisibilityHelper.calculateVisibilityTimeoutExponentially(receiveMessageCount,
+		int timeout = ErrorHandlerVisibilityHelper.calculateVisibilityTimeoutExponentially(receiveMessageCount,
 				initialVisibilityTimeoutSeconds, multiplier, maxVisibilityTimeoutSeconds);
+		return randomSupplier.get().nextInt(timeout + 1);
 	}
 
-	public static <T> Builder<T> builder() {
-		return new Builder<>();
+	public static <T> ExponentialBackoffErrorHandlerWithFullJitter.Builder<T> builder() {
+		return new ExponentialBackoffErrorHandlerWithFullJitter.Builder<>();
 	}
 
 	public static class Builder<T> {
@@ -117,31 +123,40 @@ public class ExponentialBackoffErrorHandler<T> implements AsyncErrorHandler<T> {
 		private int initialVisibilityTimeoutSeconds = BackoffVisibilityConstants.DEFAULT_INITIAL_VISIBILITY_TIMEOUT_SECONDS;
 		private double multiplier = BackoffVisibilityConstants.DEFAULT_MULTIPLIER;
 		private int maxVisibilityTimeoutSeconds = BackoffVisibilityConstants.DEFAULT_MAX_VISIBILITY_TIMEOUT_SECONDS;
+		private Supplier<Random> randomSupplier = ThreadLocalRandom::current;
 
-		public Builder<T> initialVisibilityTimeoutSeconds(int initialVisibilityTimeoutSeconds) {
+		public ExponentialBackoffErrorHandlerWithFullJitter.Builder<T> initialVisibilityTimeoutSeconds(
+				int initialVisibilityTimeoutSeconds) {
 			ErrorHandlerVisibilityHelper.checkVisibilityTimeout(initialVisibilityTimeoutSeconds);
 			this.initialVisibilityTimeoutSeconds = initialVisibilityTimeoutSeconds;
 			return this;
 		}
 
-		public Builder<T> multiplier(double multiplier) {
+		public ExponentialBackoffErrorHandlerWithFullJitter.Builder<T> multiplier(double multiplier) {
 			Assert.isTrue(multiplier >= 1,
 					() -> "Invalid multiplier '" + multiplier + "'. Should be greater than " + "or equal to 1.");
 			this.multiplier = multiplier;
 			return this;
 		}
 
-		public Builder<T> maxVisibilityTimeoutSeconds(int maxVisibilityTimeoutSeconds) {
+		public ExponentialBackoffErrorHandlerWithFullJitter.Builder<T> maxVisibilityTimeoutSeconds(
+				int maxVisibilityTimeoutSeconds) {
 			ErrorHandlerVisibilityHelper.checkVisibilityTimeout(maxVisibilityTimeoutSeconds);
 			this.maxVisibilityTimeoutSeconds = maxVisibilityTimeoutSeconds;
 			return this;
 		}
 
-		public ExponentialBackoffErrorHandler<T> build() {
+		public ExponentialBackoffErrorHandlerWithFullJitter.Builder<T> randomSupplier(Supplier<Random> randomSupplier) {
+			Assert.notNull(randomSupplier, "randomSupplier cannot be null");
+			this.randomSupplier = randomSupplier;
+			return this;
+		}
+
+		public ExponentialBackoffErrorHandlerWithFullJitter<T> build() {
 			Assert.isTrue(initialVisibilityTimeoutSeconds <= maxVisibilityTimeoutSeconds,
 					"Initial visibility timeout must not exceed max visibility timeout");
-			return new ExponentialBackoffErrorHandler<>(initialVisibilityTimeoutSeconds, multiplier,
-					maxVisibilityTimeoutSeconds);
+			return new ExponentialBackoffErrorHandlerWithFullJitter<>(initialVisibilityTimeoutSeconds, multiplier,
+					maxVisibilityTimeoutSeconds, randomSupplier);
 		}
 	}
 }
