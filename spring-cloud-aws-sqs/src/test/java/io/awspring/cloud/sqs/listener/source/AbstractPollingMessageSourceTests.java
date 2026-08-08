awspring/spring-cloud-aws

commit 287ce48df905cf09dc95ba2efbec178bb70de88b

fix: polling future removed on exception (#1455)

spring-cloud-aws-sqs/src/test/java/io/awspring/cloud/sqs/listener/source/AbstractPollingMessageSourceTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2023 the original author or authors.
+ * Copyright 2013-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -416,6 +416,47 @@ class AbstractPollingMessageSourceTests {
 			source.stop();
 			taskExecutor.shutdown();
 		}
+	}
+
+	@Test
+	void shouldRemovePollingFutureOnException() throws InterruptedException {
+		String testName = "shouldClearPollingFuturesOnException";
+
+		SemaphoreBackPressureHandler backPressureHandler = SemaphoreBackPressureHandler.builder()
+			.acquireTimeout(Duration.ofMillis(100)).batchSize(10).totalPermits(10)
+			.throughputConfiguration(BackPressureMode.ALWAYS_POLL_MAX_MESSAGES).build();
+
+		AbstractPollingMessageSource<Object, Message> source = new AbstractPollingMessageSource<>() {
+			@Override
+			protected CompletableFuture<Collection<Message>> doPollForMessages(int messagesToRequest) {
+				return CompletableFuture.failedFuture(new RuntimeException("Simulating a polling error"));
+			}
+		};
+
+		BackOffPolicy policy = mock(BackOffPolicy.class);
+		BackOffContext ctx = mock(BackOffContext.class);
+		given(policy.start(null)).willReturn(ctx);
+
+		source.setBackPressureHandler(backPressureHandler);
+		source.setMessageSink((msgs, context) -> CompletableFuture.completedFuture(null));
+		source.setId(testName + " source");
+		source.setPollingEndpointName("test-queue");
+		source.configure(SqsContainerOptions.builder().pollBackOffPolicy(policy).build());
+		source.setTaskExecutor(createTaskExecutor(testName));
+		source.setAcknowledgementProcessor(getNoOpsAcknowledgementProcessor());
+
+		@SuppressWarnings("unchecked")
+		Collection<CompletableFuture<?>> futures = (Collection<CompletableFuture<?>>) ReflectionTestUtils
+			.getField(source, "pollingFutures");
+		// Verify that the pollingFutures collection is initially empty
+		assertThat(futures).isEmpty();
+
+		source.start();
+
+		// Verify that the pollingFutures collection is empty after the exceptional completion
+		assertThat(futures).isEmpty();
+
+		source.stop();
 	}
 
 	private static boolean doAwait(CountDownLatch processingLatch) {

spring-cloud-aws-sqs/src/main/java/io/awspring/cloud/sqs/listener/source/AbstractPollingMessageSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2022 the original author or authors.
+ * Copyright 2013-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -319,7 +319,7 @@ public abstract class AbstractPollingMessageSource<T, S> extends AbstractMessage
 
 	private <F> CompletableFuture<F> managePollingFuture(CompletableFuture<F> pollingFuture) {
 		this.pollingFutures.add(pollingFuture);
-		pollingFuture.thenRun(() -> this.pollingFutures.remove(pollingFuture));
+		pollingFuture.whenComplete((result, throwable) -> this.pollingFutures.remove(pollingFuture));
 		return pollingFuture;
 	}
 

commit 7209e91d71a4ca454ba031cff37aad66ecef6bcb

test(sqs): add regression test for ConcurrentModificationException on container stop
Cancelling an in-flight polling future during stop() runs the whenComplete
callback registered in managePollingFuture inline on the stopping thread,
removing the future from pollingFutures while the collection is iterated.
The synchronizedCollection mutex is reentrant, so synchronization alone
cannot prevent this; iterating a snapshot copy does.

Regression introduced in #1455 by replacing thenRun with whenComplete,
first released in 3.4.1 and 4.0.0.

spring-cloud-aws-sqs/src/test/java/io/awspring/cloud/sqs/listener/source/AbstractPollingMessageSourceTests.java
@@ -16,6 +16,7 @@
 package io.awspring.cloud.sqs.listener.source;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.InstanceOfAssertFactories.type;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -476,6 +477,31 @@ class AbstractPollingMessageSourceTests {
 		assertThat(futures).isEmpty();
 
 		source.stop();
+	}
+
+	@Test
+	void shouldNotThrowConcurrentModificationExceptionWhenCancellingPollsOnStop() {
+		String testName = "shouldNotThrowConcurrentModificationExceptionWhenCancellingPollsOnStop";
+
+		AbstractPollingMessageSource<Object, Message> source = new AbstractPollingMessageSource<>() {
+			@Override
+			protected CompletableFuture<Collection<Message>> doPollForMessages(int messagesToRequest) {
+				return new CompletableFuture<>();
+			}
+		};
+
+		source.setId(testName + " source");
+		source.setPollingEndpointName("test-queue");
+		source.configure(SqsContainerOptions.builder().listenerShutdownTimeout(Duration.ZERO).build());
+		source.setAcknowledgementProcessor(getNoOpsAcknowledgementProcessor());
+
+		// Cancelling an in-flight poll runs the whenComplete callback registered in managePollingFuture
+		// inline on the stopping thread, removing the future from pollingFutures while it's being iterated
+		CompletableFuture<Object> pollingFuture = new CompletableFuture<>();
+		ReflectionTestUtils.invokeMethod(source, "managePollingFuture", pollingFuture);
+
+		assertThatNoException().isThrownBy(source::stop);
+		assertThat(pollingFuture).isCancelled();
 	}
 
 	private static boolean doAwait(CountDownLatch processingLatch) {